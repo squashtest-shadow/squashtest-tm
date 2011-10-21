0879ff2269063a5c49cf97c8a5e877e4ed93589e
bsiri <bsiri@localhost>
2011-10-21 11:44:33 +0200

    1.1.0 : license

--- core/org.squashtest.csp.core.web/src/main/webapp/scripts/squashtest/jquery.squashtm.datatables.js
+++ core/org.squashtest.csp.core.web/src/main/webapp/scripts/squashtest/jquery.squashtm.datatables.js
@@ -11,6 +11,7 @@
  *     (at your option) any later version.
  *
  *     this software is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU Lesser General Public License for more details.
  *
