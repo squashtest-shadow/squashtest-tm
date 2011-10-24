a625caca5096ae5059a59b1f20299d2b54bff8f0
bsiri <bsiri@localhost>
2011-10-24 11:59:03 +0200

    1.1.0 : refactoring the togglepanels

--- core/org.squashtest.csp.core.web/src/main/webapp/scripts/squashtest/jquery.squashtm.togglepanels.js
+++ core/org.squashtest.csp.core.web/src/main/webapp/scripts/squashtest/jquery.squashtm.togglepanels.js
@@ -12,67 +12,54 @@
 		
 		_create : function(){
 			var widget=this;
-				
 			
-			this.element.each(function(){
-				var jqThis = $(this);
-				var wrapper = $('<div/>', {class:"toggle-panel ui-accordion ui-widget ui-helper-reset ui-accordion-icons"});
-				var panelHead = $('<h3/>', {class:"ui-accordion-header ui-helper-reset ui-state-default ui-state-hover ui-state-active ui-corner-top"});
-				var titlepanel = $('<div/>', {style:"overflow:hidden;"});
-				var snapleft = $('<div class="snap-left"><a class="tg-link" href="#"></a></div>');
-				var snapright = $('<div/>', {class:"snap-right"});
-
-				jqThis.addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active");
-				titlepanel.append(snapleft);
-				titlepanel.append(snapright);
-				panelHead.append(titlepanel);
-				
-				wrapper.append(panelHead);
-				
-				wrapper.insertBefore(jqThis);	
-				wrapper.append(jqThis);
-				
-				panelHead.click($.proxy(function(event){
-					event.stopImmediatePropagation();
-					widget.toggleContent.call(this);
-				}, jqThis));
-			});
+			var wrapper = $('<div/>', {class:"toggle-panel ui-accordion ui-widget ui-helper-reset ui-accordion-icons"});
+			var panelHead = $('<h3/>', {class:"ui-accordion-header ui-helper-reset ui-state-default ui-state-hover ui-state-active ui-corner-top"});
+			var titlepanel = $('<div/>', {style:"overflow:hidden;"});
+			var snapleft = $('<div class="snap-left"><a class="tg-link" href="#"></a></div>');
+			var snapright = $('<div/>', {class:"snap-right"});			
+			this.element.addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active");				
+		
+			titlepanel.append(snapleft);
+			titlepanel.append(snapright);
+			panelHead.append(titlepanel);
 			
+			this.element.wrap(wrapper);
+			panelHead.insertBefore(this.element);
+			
+			panelHead.click($.proxy(function(event){
+				event.stopImmediatePropagation();
+				widget.toggleContent.call(this.element);
+			}, this));
+		
 		}, 
 		
 		_init : function(){
 			var settings = this.options;
-			try{
-			this.element.each(function(){
-				var jqThis = $(this);
-				var panelHead = jqThis.prev();
-				
-				if (settings.initiallyOpen){
-					panelHead.addClass('tg-open');
-				}else{
-					jqThis.hide();
-					panelHead.toggleClass('ui-state-active ui-corner-top ui-corner-all');
-				}
-				
-				panelHead.find(".snap-left a").text(settings.title);
-				
-				panelHead.parent().addClass(settings.cssClasses);
-				
-				if (settings.panelButtonsSelector){
-					var inputs = $(settings.panelButtonsSelector);
-					inputs.click(function(event){event.stopPropagation();});
-					panelHead.find('.snap-right').append(inputs);
-				}
-				
-			});
-			}catch(e){alert('exception on create');}
+			
+			var panelHead = this.element.prev();
+			if (settings.initiallyOpen){
+				panelHead.addClass('tg-open');
+			}else{
+				this.element.hide();
+				panelHead.toggleClass('ui-state-active ui-corner-top ui-corner-all');
+			}		
+			
+			panelHead.find(".snap-left a").text(settings.title);				
+			panelHead.parent().addClass(settings.cssClasses);
+			
+			if (settings.panelButtonsSelector){
+				var inputs = $(settings.panelButtonsSelector);
+				inputs.click(function(event){event.stopPropagation();});
+				panelHead.find('.snap-right').append(inputs);
+			}	
 		},
 		
 		toggleContent : function(){
 			var panelHead = this.prev();
 			
 			if (! panelHead.length){
-				return; 	//if the head is not found, that's probably because the body was detached due to the animating sequence.
+				return; 	//if the head is not found, that's usually because the body was detached due to the animating sequence. Sorry for this but the 			(:animated) selector wouldn't work
 			}
 			
 			this.toggle('blind', 500);			
@@ -84,7 +71,5 @@
 		}
 	
 	});
-	
-	
 	
 })(jQuery)
