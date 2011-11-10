969db037b1b2aaefa73a3b349ea62b533a71e5b5
gfouquet <gfouquet@localhost>
2011-11-10 12:10:47 +0100

    [US 384-01] Created a i18n'd TestCaseWeight enum

---
+++ tm/org.squashtest.csp.tm.service/src/main/java/org/squashtest/csp/tm/domain/testcase/TestCaseWeight.java
@@ -0,0 +1,49 @@
+/**
+ *     This file is part of the Squashtest platform.
+ *     Copyright (C) 2010 - 2011 Squashtest TM, Squashtest.org
+ *
+ *     See the NOTICE file distributed with this work for additional
+ *     information regarding copyright ownership.
+ *
+ *     This is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     this software is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.squashtest.csp.tm.domain.testcase;
+
+import org.squashtest.csp.tm.domain.Internationalizable;
+
+/**
+ * 
+ * @author Gregory Fouquet
+ *
+ */
+public enum TestCaseWeight implements Internationalizable, Comparable<TestCaseWeight> {
+	VERY_HIGH(1), HIGH(2), MEDIUM(3), LOW(4);
+	
+	private static final String I18N_KEY_ROOT = "test-case.weight.";
+
+	private final int value;
+	
+	private TestCaseWeight(int value) {
+		this.value = value;
+	}
+
+	/** (non-Javadoc)
+	 * @see org.squashtest.csp.tm.domain.Internationalizable#getI18nKey()
+	 */
+	@Override
+	public String getI18nKey() {
+		return I18N_KEY_ROOT + this.name();
+	}
+}
---
+++ tm/org.squashtest.csp.tm.service/src/test/groovy/org/squashtest/csp/tm/domain/testcase/TestCaseWeightTest.groovy
@@ -0,0 +1,48 @@
+/**
+ *     This file is part of the Squashtest platform.
+ *     Copyright (C) 2010 - 2011 Squashtest TM, Squashtest.org
+ *
+ *     See the NOTICE file distributed with this work for additional
+ *     information regarding copyright ownership.
+ *
+ *     This is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     this software is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.squashtest.csp.tm.domain.testcase;
+
+import static org.squashtest.csp.tm.domain.testcase.TestCaseWeight.*;
+import spock.lang.Specification;
+import spock.lang.Unroll;
+
+/**
+ * @author Gregory Fouquet
+ *
+ */
+class TestCaseWeightTest extends Specification {
+	@Unroll("i18n key of #status should be '#key'")
+	def "should return i18n key"() {
+		when:
+		def actualKey = status.i18nKey
+		
+		then:
+		actualKey == key
+		
+		where:
+		status    | key
+		LOW       | "test-case.weight.LOW"
+		MEDIUM    | "test-case.weight.MEDIUM"
+		HIGH      | "test-case.weight.HIGH"
+		VERY_HIGH | "test-case.weight.VERY_HIGH"
+	}
+}
