9aa9b02868b94e71a1ffd099adf991bee9b58dfc
bsiri <bsiri@localhost>
2011-11-18 18:53:40 +0100

    sprint 25 : task 385-01, debugged and added ITs for RequirementAuditEventDao. The cascade suppression of events when deleting a requirement is still to be tested (RequirementDeletionHandlerIT needs implementation)

--- tm/org.squashtest.csp.tm.service/src/main/java/org/squashtest/csp/tm/internal/repository/hibernate/HibernateRequirementAuditEventDao.java
+++ tm/org.squashtest.csp.tm.service/src/main/java/org/squashtest/csp/tm/internal/repository/hibernate/HibernateRequirementAuditEventDao.java
@@ -1,8 +1,29 @@
+/**
+ *     This file is part of the Squashtest platform.
+ *     Copyright (C) 2010 - 2011 Squashtest TM, Squashtest.org
+ *
+ *     See the NOTICE file distributed with this work for additional
+ *     information regarding copyright ownership.
+ *
+ *     This is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     this software is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.squashtest.csp.tm.internal.repository.hibernate;
 
 import java.util.List;
 
 import org.hibernate.Query;
+import org.hibernate.type.LongType;
 import org.springframework.stereotype.Repository;
 import org.squashtest.csp.tm.domain.event.RequirementAuditEvent;
 import org.squashtest.csp.tm.internal.repository.RequirementAuditEventDao;
@@ -21,7 +42,7 @@ public class HibernateRequirementAuditEventDao extends
 
 
 	@Override
-	public List<RequirementAuditEvent> findAllByRequirementId(List<Long> requirementIds) {
+	public List<RequirementAuditEvent> findAllByRequirementIds(List<Long> requirementIds) {
 		SetQueryParametersCallback callback = new EventByRequirementListCallback(requirementIds);
 		return executeListNamedQuery("requirementAuditEvent.findAllByRequirementIdList", callback);
 	}
@@ -38,7 +59,7 @@ public class HibernateRequirementAuditEventDao extends
 		
 		@Override
 		public void setQueryParameters(Query query) {
-			query.setParameter("requirementIds", requirementIdsList);
+			query.setParameterList("requirementIds", requirementIdsList, LongType.INSTANCE);
 		}
 		
 		
