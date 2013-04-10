51173f69c989a939f9304c25f4d64cf18f08098f
flaurens <flaurens@localhost>
2013-04-10 15:29:09 +0200

    Test suites now sorted alphabetically in execution progress report

--- plugins/plugin.report.std/src/main/java/org/squashtest/tm/internal/domain/report/common/dto/ExProgressIterationDto.java
+++ plugins/plugin.report.std/src/main/java/org/squashtest/tm/internal/domain/report/common/dto/ExProgressIterationDto.java
@@ -20,6 +20,8 @@
  */
 package org.squashtest.tm.internal.domain.report.common.dto;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -50,9 +52,16 @@ public class ExProgressIterationDto extends ExProgressScheduledAbstractDto {
 		for (TestSuite testSuite : iteration.getTestSuites()) {
 			ExProgressTestSuiteDto testSuiteDto = new ExProgressTestSuiteDto(testSuite);
 			testSuites.add(testSuiteDto);
+			Collections.sort(testSuites, new testSuiteComparator());
 		}
 	}
 
+	private static class testSuiteComparator implements Comparator<ExProgressTestSuiteDto>{
+	    @Override
+		public int compare(ExProgressTestSuiteDto suite1, ExProgressTestSuiteDto suite2) {
+	        return suite1.getName().compareTo(suite2.getName());
+	    }
+	}
 	public List<ExProgressTestPlanDto> getTestPlans() {
 		return testPlans;
 	}
