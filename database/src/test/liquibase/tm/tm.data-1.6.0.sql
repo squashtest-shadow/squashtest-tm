ceaf37d386f114056b4c334dcca89becc1faa2e7
flaurens <flaurens@localhost>
2013-05-31 17:29:05 +0200

    Added a migration test for [Issue 2334]

---
+++ database/src/test/liquibase/tm/tm.data-1.6.0.sql
@@ -0,0 +1,9 @@
+INSERT INTO `CORE_PARTY` (`PARTY_ID`) VALUES
+(10),
+(11),
+(12);
+
+INSERT INTO `CORE_TEAM` (`PARTY_ID`, `NAME`, `DESCRIPTION`, `CREATED_BY`, `CREATED_ON`, `LAST_MODIFIED_BY`, `LAST_MODIFIED_ON`) VALUES
+(10, 'team1', 'this is team1', 'guest_tpl', '2011-06-21 08:40:32', 'guest_tpl', '2011-06-21 08:41:00'),
+(11, 'team2', 'this is team2', 'guest_tpl', '2011-06-21 08:40:32', 'guest_tpl', '2011-06-21 08:41:00'),
+(12, 'team3', 'this is team3', 'guest_tpl', '2011-06-21 08:40:32', 'guest_tpl', '2011-06-21 08:41:00');
