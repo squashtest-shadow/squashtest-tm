d6db9efecbc8845799b7bbf4f48235c25aed27c2
mpagnon <mpagnon@localhost>
2013-10-03 11:46:12 +0200

    [Feat 2247] fixed js

--- tm/tm.web/src/main/webapp/scripts/iteration-management/test-suite-menu/TestSuiteMenu.js
+++ tm/tm.web/src/main/webapp/scripts/iteration-management/test-suite-menu/TestSuiteMenu.js
@@ -18,74 +18,77 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define([ "jquery", "underscore", "jqueryui"], function($,_) {
+define([ "jquery", "underscore", "jqueryui" ],function($, _) {
 
 	function TestSuiteMenuNewStatuses() {
-		
+
 		var initiallyChecked = [];
 		var checkedIds = [];
 		var initiallyNotChecked = [];
 		var uncheckedIds = [];
-		
+
 		this.getChecked = function() {
 			return this.checkedIds;
 		};
+		
 		this.getUnchecked = function() {
 			return this.uncheckedIds;
 		};
+		
 		this.reset = function(checked, notChecked) {
 			this.intiallyChecked = checked;
 			this.initiallyNotChecked = notChecked;
 			this.checkedIds = [];
 			this.uncheckedIds = [];
 		};
-		
+
 		this.change = function(id, checked) {
-			if(checked){
-				this.unChekedIds = _.reject(this.unChekedIds, function(unchekedId){
-				return unchekedId === id;
-				});				
-				if(!_.contains(this.initiallyChecked, id)){
+			if (checked) {
+				this.unChekedIds = _.reject(this.unChekedIds, function(
+						unchekedId) {
+					return unchekedId === id;
+				});
+				if (!_.contains(this.initiallyChecked, id)) {
 					this.checkedIds.push(id);
 				}
-			}else{
-				this.checkedIds = 	_.reject(this.checkedIds, function(checkedId){
+			} else {
+				this.checkedIds = _.reject(this.checkedIds, function(
+						checkedId) {
 					return checkedId === id;
-					});	
-				if(!_.contains(this.initiallyNotChecked, id)){
+				});
+				if (!_.contains(this.initiallyNotChecked, id)) {
 					this.uncheckedIds.push(id);
 				}
 			}
 		};
-		
-		this.add = function(id){
+
+		this.add = function(id) {
 			this.initiallyNotChecked.push(id);
 			this.checkedIds.push(id);
-		}
-		
+		};
+
 	}
-	
+
 	function initWidgets() {
-		
+
 		$("#manage-test-suites-buttonmenu").buttonmenu({
 			anchor : 'right',
 			'no-auto-hide' : true
 		});
-		
-		$("#suite-manager-menu-ok-button, #suite-manager-menu-cancel-button").button();	
-		
+
+		$("#suite-manager-menu-ok-button, #suite-manager-menu-cancel-button")
+				.button();
+
 		$("#suite-manager-menu-button").squashButton({
 			'text' : false,
 			icons : {
 				primary : 'ui-icon-circle-plus'
 			}
 		});
-		
 
 		this.menucontrol = $("#manage-test-suites-buttonmenu");
 		this.menu = $("#manage-test-suites-menu");
 	}
-
 
 	function TestSuiteMenu(settings) {
 
@@ -130,14 +133,14 @@ define([ "jquery", "underscore", "jqueryui"], function($,_) {
 		};
 
 		var initializeContent = $.proxy(function() {
-			
+
 			// wipe the previous items
 			this.menu.find('li.suite-item').remove();
-			
+
 			// generate new content
 			var model = this.model.getData();
 			var items = [];
-			
+
 			for ( var i in model) {
 				var node = makeItem(model[i]);
 				items.push(node);
@@ -149,65 +152,65 @@ define([ "jquery", "underscore", "jqueryui"], function($,_) {
 				var textB = getItemDomText(b.get(0));
 				return (textA < textB) ? -1 : 1;
 			});
-			
-			//append to the list
-			
+
+			// append to the list
+
 			this.menu.prepend(sorted);
-			
-			
 
 		}, this);
 
 		var getCheckboxes = $.proxy(function() {
 			return this.menu.find('input[name="menu-suite-item"]');
 		}, this);
-		
-		var getCheckboxBySuiteId = $.proxy(function(id){
-			return this.menu.find('input#menu-suite-'+id);
+
+		var getCheckboxBySuiteId = $.proxy(function(id) {
+			return this.menu.find('input#menu-suite-' + id);
 		}, this);
 
 		var getDatatableSelected = $.proxy(function() {
-			return $(this.datatableSelector).squashTable().getSelectedIds();
+			return $(this.datatableSelector).squashTable()
+					.getSelectedIds();
 		}, this);
-		
-		
+
 		var displayAddSuiteError = $.proxy(function(xhr, text) {
-			try {
-				var errContent = jQuery.parseJSON(xhr.responseText);
-				var message = $("<div/>", {
-					'margin-top' : 'auto',
-					'margin-bottom' : 'auto'
-				});
+				try {
+					var errContent = jQuery.parseJSON(xhr.responseText);
+					var message = $("<div/>", {
+						'margin-top' : 'auto',
+						'margin-bottom' : 'auto'
+					});
 
-				if (errContent.fieldValidationErrors !== undefined) {
-					var errors = errContent.fieldValidationErrors;
-					for ( var i = 0; i < errors.length; i++) {
-						message.append("<div>" + errors[0].errorMessage + "</div>");
+					if (errContent.fieldValidationErrors !== undefined) {
+						var errors = errContent.fieldValidationErrors;
+						for ( var i = 0; i < errors.length; i++) {
+							message.append("<div>"+ errors[0].errorMessage+ "</div>");
+						}
+					} else {
+						message.append('<div>could not add your suite : unexpected error</div>');
 					}
-				} else {
-					message.append('<div>could not add your suite : unexpected error</div>');
+
+					oneShotDialog('Information', message);
+
+				} catch (wtf) {
+					// non json error : it must be handled
+					// by the generic handler (see the
+					// red thing showing up in the view
+					// right now)
+					// anyway, job done here
 				}
-
-				oneShotDialog('Information', message);
-
-			} catch (wtf) {
-				// non json error : it must be handled
-				// by the generic handler (see the
-				// red thing showing up in the view
-				// right now)
-				// anyway, job done here
-			}
-		}, this);
+			}, this);
 
 		/*
-		 * **************************** public ******************************
+		 * **************************** public
+		 * ******************************
 		 */
 
 		this.update = function(evt) {
-			if ((evt === undefined) || (evt.evt_name == "node.rename") || (evt.evt_name == "node.remove") ||
-					(evt.evt_name == "node.refresh")) {
+			if ((evt === undefined) || (evt.evt_name == "node.rename")
+					|| (evt.evt_name == "node.remove")
+					|| (evt.evt_name == "node.refresh")) {
 				initializeContent();
-				
+
 			} else if (evt.evt_name == "node.add") {
 				addSuiteToMenuContent(evt);
 			}
@@ -229,51 +232,48 @@ define([ "jquery", "underscore", "jqueryui"], function($,_) {
 			this.model.postNew(name).error(displayAddSuiteError);
 		}, this);
 
-		
 		// -------- binding -------------
-		
-		
+
 		var bindCheckboxes = $.proxy(function(evt) {
 			var self = this;
-			this.menu.delegate('input:checkbox', 'change', function(evt) {
-				var checkbx = $(evt.currentTarget);
-					self.testSuiteNewStatuses.change(checkbx.data('suite-id'), checkbx.is(":checked"));
+			this.menu.delegate('input:checkbox', 'change',
+					function(evt) {
+						var checkbx = $(evt.currentTarget);
+						self.testSuiteNewStatuses.change(checkbx
+								.data('suite-id'), checkbx
+								.is(":checked"));
 
-			});
+					});
 		}, this);
 
 		var bindOkButton = $.proxy(function() {
-			var self = this;
-			$('#suite-manager-menu-ok-button').on('click', function(evt) {
-				if (!self.testPlanItemIds.length) {
-					$(settings.emptySelectionMessageSelector).openMessage();
-				} else {
-					var toSend = {};
-					var checkedSuiteIds = self.testSuiteNewStatuses.getChecked();
-					var uncheckedSuiteIds = self.testSuiteNewStatuses.getUnchecked();
-					if (uncheckedSuiteIds.length < 1 && checkedSuiteIds.length < 1) {
-						//TODO change message ??
-						$(settings.emptySuiteSelectionMessageSelector).openMessage();
-					} else {
-						toSend['bound-test-suites'] = checkedSuiteIds;
-						toSend['unbound-test-suites'] = uncheckedSuiteIds;
-						toSend['test-plan-items'] = self.testPlanItemIds;
-						//TODO bind controller
-						self.model.postBindChanged(toSend).success(function() {
-							self.menucontrol.buttonmenu('close');
-						});
-					}
-				}
-			});
-		}, this);
+				var self = this;
+				$('#suite-manager-menu-ok-button').on('click',function(evt) {
+						if (!self.testPlanItemIds.length) {
+							$(settings.emptySelectionMessageSelector).openMessage();
+						} else {
+							var toSend = {};
+							var checkedSuiteIds = self.testSuiteNewStatuses.getChecked();
+							var uncheckedSuiteIds = self.testSuiteNewStatuses.getUnchecked();
+							if (uncheckedSuiteIds.length > 0 && checkedSuiteIds.length > 0) {
+								toSend['bound-test-suites'] = checkedSuiteIds;
+								toSend['unbound-test-suites'] = uncheckedSuiteIds;
+								toSend['test-plan-items'] = self.testPlanItemIds;
+								self.model.postBindChanged(toSend).success(function() {
+										self.menucontrol.buttonmenu('close');
+								});
+							}
+						}
+					});
+			}, this);
 
 		var bindCancelButton = $.proxy(function() {
 			var self = this;
-			$('#suite-manager-menu-cancel-button').on('click', function(evt) {
-				self.menucontrol.buttonmenu('close');
-			});
+			$('#suite-manager-menu-cancel-button').on('click',
+					function(evt) {
+						self.menucontrol.buttonmenu('close');
+					});
 		}, this);
-
 
 		var bindAddButton = $.proxy(function() {
 			$('#suite-manager-menu-button').on('click', function(evt) {
@@ -284,8 +284,9 @@ define([ "jquery", "underscore", "jqueryui"], function($,_) {
 		var bindInput = $.proxy(function() {
 			var input = $('#suite-manager-menu-input');
 			input.on('keydown', function(evt) {
-				if (evt.which == '8'){
-					evt.stopImmediatePropagation();//backspace will nagivate to previous page if not canceled here
+				if (evt.which == '8') {
+					evt.stopImmediatePropagation();
+					// backspace will nagivate to previous page if not canceled here
 				}
 				if (evt.which == '13') {
 					addSuite();
@@ -294,65 +295,61 @@ define([ "jquery", "underscore", "jqueryui"], function($,_) {
 			});
 		}, this);
 
-		
-
 		var bindShowMenuButton = $.proxy(function() {
-			var self = this;
-			$("#manage-test-suites-buttonmenu").on('click', function(evt){
-				self.testPlanItemIds = getDatatableSelected();
-				if (!self.testPlanItemIds.length) {
-					// no item selected : close menu and warn
-					self.menucontrol.buttonmenu('close');
-					evt.stopImmediatePropagation();
-					$(settings.emptySelectionMessageSelector).openMessage();
-					
-				}else{		
-					//TODO init with rightfull datas
-					var suites = self.model.getData();
-					
-					//get item ids by suite id
-					var itemIdsBySuite = {};
-					_.each(suites, function(suite){
-						itemIdsBySuite[suite.id] = [];
-					});
-					
-					_.each(self.testPlanItemIds, function(itemId){ 
-						var itemSuiteIds =  $(self.datatableSelector).squashTable().getDataById(itemId)["suiteIds"];
-						_.each(itemSuiteIds, function(itemSuiteId){
-							itemIdsBySuite[itemSuiteId].push(itemId);
-						});
-					});
-					
-					//compute statuses and update checkboxes
-					var suiteStatuses = {};
-					var unboundIds = [];
-					var boundIds = [];
-					getCheckboxes().prop('indeterminate', false); // reset the checkboxes
-					_.each(itemIdsBySuite, function(itemIds, suiteId){
-						var status;
-						var checkbox = getCheckboxBySuiteId(suiteId);
-						if(itemIds.length == self.testPlanItemIds.length){
-							status = "checked";
-							boundIds.push(suiteId);
-							checkbox.prop('checked', true);
-						}else if(itemIds.length === 0 ){
-							status = "unchecked";
-							unboundIds.push(suiteId);
-							checkbox.prop('checked', false);
-						}else{
-							status = "undefined";
-							checkbox.prop("indeterminate", true);
-						}
-						suiteStatuses[suiteId] = status;						
-					})
-					
-					self.testSuiteNewStatuses.reset(boundIds, unboundIds); // reset the model
-					$("#suite-manager-menu-input").val(""); // reset the input field
-				}
-				
-			});
-		}, this);
-		
+				var self = this;
+				$("#manage-test-suites-buttonmenu").on('click',function(evt) {
+					self.testPlanItemIds = getDatatableSelected();
+					if (!self.testPlanItemIds.length) {
+						// no item selected: close menu and warn
+						self.menucontrol.buttonmenu('close');
+						evt.stopImmediatePropagation();
+						$(settings.emptySelectionMessageSelector).openMessage();
+
+					} else {
+						var suites = self.model.getData();
+
+						// get item ids by suite id
+						var itemIdsBySuite = {};
+						_.each(suites,function(suite) {
+								itemIdsBySuite[suite.id] = [];
+							});
+
+						_.each(self.testPlanItemIds,function(itemId) {
+								var itemSuiteIds = $(self.datatableSelector).squashTable().getDataById(itemId)["suiteIds"];
+								_.each(itemSuiteIds,function(itemSuiteId) {
+										itemIdsBySuite[itemSuiteId].push(itemId);
+									});
+							});
+
+						// compute statuses and update checkboxes
+						var suiteStatuses = {};
+						var unboundIds = [];
+						var boundIds = [];
+						getCheckboxes().prop('indeterminate',false); // reset the checkboxes
+						_.each(itemIdsBySuite,function(itemIds, suiteId) {
+								var status;
+								var checkbox = getCheckboxBySuiteId(suiteId);
+								if (itemIds.length == self.testPlanItemIds.length) {
+									status = "checked";
+									boundIds.push(suiteId);
+									checkbox.prop('checked',true);
+								} else if (itemIds.length === 0) {
+									status = "unchecked";
+									unboundIds.push(suiteId);
+									checkbox.prop('checked',false);
+								} else {
+									status = "undefined";
+									checkbox.prop("indeterminate",true);
+								}
+								suiteStatuses[suiteId] = status;
+							});
+
+						self.testSuiteNewStatuses.reset(boundIds, unboundIds); // reset the model
+						$("#suite-manager-menu-input").val(""); // reset the input field
+					}
+				});
+			}, this);
+
 		var initHandlerBinding = $.proxy(function() {
 			bindShowMenuButton();
 			bindAddButton();
@@ -362,18 +359,17 @@ define([ "jquery", "underscore", "jqueryui"], function($,_) {
 			bindCheckboxes();
 		}, this);
 
-
 		/* *********************** init ********************* */
 
 		this.datatableSelector = settings.datatableSelector;
 		this.model = settings.model;
 		this.model.addListener(this);
-		
-		this.instance = $(settings.instanceSelector);		
+
+		this.instance = $(settings.instanceSelector);
 		this.testSuiteNewStatuses = new TestSuiteMenuNewStatuses();
-		
-		initWidgets.call(this);				
-		
+
+		initWidgets.call(this);
+
 		initHandlerBinding();
 
 		initializeContent();
