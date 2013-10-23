c7b608e8cf1f10e017281efd166adb03124f29cb
bfranchet <bfranchet@localhost>
2013-10-23 18:38:35 +0200

    Fix integration test

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/hibernate/package-info.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/hibernate/package-info.java
@@ -116,7 +116,7 @@
 		@NamedQuery(name = "testSuite.findAllByIterationId", query = "select ts from TestSuite ts join ts.iteration i where i.id = ?"),
 		@NamedQuery(name = "testSuite.findLaunchableTestPlan", query = "select tp from TestSuite ts join ts.testPlan tp join tp.testSuites tss where ts.id = ? and ? = tss.id and ((tp.referencedTestCase is not null) or (tp.executions is not empty)) order by index(tp)"),
 		@NamedQuery(name = "testSuite.countTestPlansFiltered", query = "select count(tps) from TestSuite ts join ts.testPlan tps where ts.id = :suiteId and tps.user.login = :userLogin"),
-		@NamedQuery(name = "testSuite.findProjectIdBySuiteId", query  = "select project.id from TestSuite ts join ts.iteration it join it.campaign camp join camp.project project where ts.id = ?1"),
+		@NamedQuery(name = "testSuite.findProjectIdBySuiteId", query  = "select project.id from TestSuite ts join ts.iteration it join it.campaign camp join camp.project project where ts.id = ?"),
 		//TestCase
 		@NamedQuery(name = "testCase.findAllByIdListOrderedByName", query = "from TestCase tc where id in (:testCasesIds) order by tc.name asc"),
 		@NamedQuery(name = "testCase.findById", query = "from TestCase tc left join fetch tc.steps left join fetch tc.requirementVersionCoverages where tc.id = :testCaseId"),
