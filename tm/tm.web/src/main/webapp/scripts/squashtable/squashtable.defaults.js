7aa4d1df83f3ccc88e5991ff2c35ae3b006b2856
bsiri <bsiri@localhost>
2013-10-24 18:48:56 +0200

    [random bug] missing dependencies in squashtable.defaults.js

--- tm/tm.web/src/main/webapp/scripts/squashtable/squashtable.defaults.js
+++ tm/tm.web/src/main/webapp/scripts/squashtable/squashtable.defaults.js
@@ -18,7 +18,7 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(function(){
+define(['jquery'], function($){
 	
 	if (squashtm && squashtm.datatable && squashtm.datatable.defaults){
 		return squashtm.datatable.defaults;
