cc8eac43226d1d88e30ded10114e291ad990050d
mpagnon <mpagnon@localhost>
2013-11-19 18:56:15 +0100

    revert 250b821338f8 but bug still need to be fixed properly

--- tm/tm.web/src/main/webapp/scripts/test-suite-management/test-plan-panel/main.js
+++ tm/tm.web/src/main/webapp/scripts/test-suite-management/test-plan-panel/main.js
@@ -53,9 +53,7 @@ define(['squash.translator', './table', './popups'], function(translator, table,
 				FAILURE : "execution.execution-status.FAILURE",
 				SUCCESS : "execution.execution-status.SUCCESS",
 				RUNNING : "execution.execution-status.RUNNING",
-				READY	: "execution.execution-status.READY",
-				WARNING	: "execution.execution-status.WARNING",
-				ERROR	: "execution.execution-status.ERROR"
+				READY	: "execution.execution-status.READY"
 			},
 			automatedExecutionTooltip : "label.automatedExecution",
 			labelOk : "label.Ok",
