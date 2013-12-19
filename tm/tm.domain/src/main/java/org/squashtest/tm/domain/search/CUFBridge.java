3e2092f0b2ea52d979ced39cd102119488de84b6
gfouquet <gfouquet@henix.fr>
2013-12-19 15:34:01 +0100

    [Issue 3113] null custom field values are no longer indexed. This was introduced in 5b4a91ccd511 Also replaced a printStackTrace by a Logger.debug Also prevented to attept to parse dates when ethey are null or empty

--- tm/tm.domain/src/main/java/org/squashtest/tm/domain/search/CUFBridge.java
+++ tm/tm.domain/src/main/java/org/squashtest/tm/domain/search/CUFBridge.java
@@ -26,6 +26,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.hibernate.Criteria;
@@ -34,6 +35,8 @@ import org.hibernate.criterion.Restrictions;
 import org.hibernate.criterion.SimpleExpression;
 import org.hibernate.search.bridge.LuceneOptions;
 import org.hibernate.search.bridge.ParameterizedBridge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.squashtest.tm.domain.customfield.BindableEntity;
 import org.squashtest.tm.domain.customfield.CustomFieldValue;
 import org.squashtest.tm.domain.customfield.InputType;
@@ -41,6 +44,7 @@ import org.squashtest.tm.domain.requirement.RequirementVersion;
 import org.squashtest.tm.domain.testcase.TestCase;
 
 public class CUFBridge extends SessionFieldBridge implements ParameterizedBridge {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CUFBridge.class);
 
 	private SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd");
 	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
@@ -63,17 +67,18 @@ public class CUFBridge extends SessionFieldBridge implements ParameterizedBridge
 			id = requirement.getId();
 			entityType = BindableEntity.REQUIREMENT_VERSION;
 		}
-		Criteria crit = session.createCriteria(CustomFieldValue.class).createAlias("binding", "binding").createAlias("binding.customField","cuf");
+		Criteria crit = session.createCriteria(CustomFieldValue.class).createAlias("binding", "binding")
+				.createAlias("binding.customField", "cuf");
 		crit.add(Restrictions.eq("boundEntityId", id)).add(Restrictions.eq("boundEntityType", entityType));
-		
+
 		SimpleExpression typeDropDownList = Restrictions.eq("cuf.inputType", InputType.DROPDOWN_LIST);
-		if(inputType != null &&  inputType.equals(InputType.DROPDOWN_LIST.name())){
+		if (inputType != null && inputType.equals(InputType.DROPDOWN_LIST.name())) {
 			crit.add(typeDropDownList);
-		}else{
+		} else {
 			crit.add(Restrictions.not(typeDropDownList));
 		}
 		return crit.list();
-		
+
 	}
 
 	@Override
@@ -97,17 +102,10 @@ public class CUFBridge extends SessionFieldBridge implements ParameterizedBridge
 			InputType inputType = cufValue.getBinding().getCustomField().getInputType();
 			String code = cufValue.getBinding().getCustomField().getCode();
 			String val = null;
-			
+
 			switch (inputType) {
 			case DATE_PICKER:
-				Date inputDate = null;
-				try {
-					inputDate = inputFormat.parse(cufValue.getValue());
-					val = dateFormat.format(inputDate);
-
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
+				val = formatDate(cufValue);
 				break;
 			case DROPDOWN_LIST:
 				val = cufValue.getValue();
@@ -118,12 +116,40 @@ public class CUFBridge extends SessionFieldBridge implements ParameterizedBridge
 			default:
 				val = cufValue.getValue();
 			}
-			
-			Field field = new Field(code, val, luceneOptions.getStore(), luceneOptions.getIndex(),
-					luceneOptions.getTermVector());
-			field.setBoost(luceneOptions.getBoost());
-			document.add(field);
+
+			if (val != null) {
+				Field field = new Field(code, val, luceneOptions.getStore(), luceneOptions.getIndex(),
+						luceneOptions.getTermVector());
+				field.setBoost(luceneOptions.getBoost());
+				document.add(field);
+			}
 		}
+	}
+
+	/**
+	 * Formats a DATE cuf value
+	 * 
+	 * @param fieldValue
+	 * @return
+	 */
+	private String formatDate(CustomFieldValue fieldValue) {
+		String value = fieldValue.getValue();
+		
+		if (StringUtils.isEmpty(value)) {
+			// wont parse as date, early exit
+			return null;
+		}
+		
+		try {
+			Date inputDate = inputFormat.parse(fieldValue.getValue());
+			return dateFormat.format(inputDate);
+		} catch (ParseException e) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Cannot parse as date custom field of value '" + fieldValue.getValue() + '\'', e);
+			}
+		}
+		
+		return null;
 	}
 
 }
