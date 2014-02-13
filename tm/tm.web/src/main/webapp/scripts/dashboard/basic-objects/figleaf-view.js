140cd07ca88ecbd61702401466f61cf6a31228e1
mpagnon <mpagnon@localhost>
2014-02-13 15:36:09 +0100

    [Task 3167] change year of copyright notice

--- tm/tm.web/src/main/webapp/scripts/dashboard/basic-objects/figleaf-view.js
+++ tm/tm.web/src/main/webapp/scripts/dashboard/basic-objects/figleaf-view.js
@@ -1,6 +1,6 @@
 /*
  *     This file is part of the Squashtest platform.
- *     Copyright (C) 2010 - 2013 Henix, henix.fr
+ *     Copyright (C) 2010 - 2014 Henix, henix.fr
  *
  *     See the NOTICE file distributed with this work for additional
  *     information regarding copyright ownership.
@@ -18,7 +18,6 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 /*
  * A fig leaf view is mostly used to hide the charts when there are no model to back them up. Basically it is something that will display one content 
  * until the model triggers an event, then it will switch to the other content and stay that way.  
