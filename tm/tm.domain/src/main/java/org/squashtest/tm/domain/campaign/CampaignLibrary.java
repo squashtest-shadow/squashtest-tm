356ca9b1eecf9a89ad0557c53e0643bdaba8a53a
flaurens <flaurens@localhost>
2014-03-04 16:51:11 +0100

    [Feat 3209] Fix after rebase

--- tm/tm.domain/src/main/java/org/squashtest/tm/domain/campaign/CampaignLibrary.java
+++ tm/tm.domain/src/main/java/org/squashtest/tm/domain/campaign/CampaignLibrary.java
@@ -28,6 +28,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
