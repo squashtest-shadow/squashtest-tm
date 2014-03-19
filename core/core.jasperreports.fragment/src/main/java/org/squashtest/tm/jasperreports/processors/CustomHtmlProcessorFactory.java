bd8b7c5e35efb78fe174d2536a218e6bc513f319
mpagnon <mpagnon@localhost>
2014-03-19 10:24:12 +0100

    [Issue 2411] overriden JEditorPaneHtmlMarkupProcessor.convert()

--- core/core.jasperreports.fragment/src/main/java/org/squashtest/tm/jasperreports/processors/CustomHtmlProcessorFactory.java
+++ core/core.jasperreports.fragment/src/main/java/org/squashtest/tm/jasperreports/processors/CustomHtmlProcessorFactory.java
@@ -22,15 +22,38 @@ package org.squashtest.tm.jasperreports.processors;
 
 import java.awt.font.TextAttribute;
 import java.text.AttributedCharacterIterator.Attribute;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import javax.swing.JEditorPane;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AbstractDocument.LeafElement;
 import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTML.Tag;
+import javax.swing.text.html.HTMLDocument.RunElement;
 
+import net.sf.jasperreports.engine.JRPrintHyperlink;
+import net.sf.jasperreports.engine.base.JRBasePrintHyperlink;
+import net.sf.jasperreports.engine.type.HyperlinkTypeEnum;
 import net.sf.jasperreports.engine.util.JEditorPaneHtmlMarkupProcessor;
+import net.sf.jasperreports.engine.util.JRStyledText;
+import net.sf.jasperreports.engine.util.JRStyledText.Run;
+import net.sf.jasperreports.engine.util.JRStyledTextParser;
+import net.sf.jasperreports.engine.util.JRTextAttribute;
 import net.sf.jasperreports.engine.util.MarkupProcessor;
 import net.sf.jasperreports.engine.util.MarkupProcessorFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -46,6 +69,7 @@ public class CustomHtmlProcessorFactory extends JEditorPaneHtmlMarkupProcessor i
 
 	private static CustomHtmlProcessorFactory custom_instance;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomHtmlProcessorFactory.class);
 	
 
 	@Override
@@ -89,4 +113,189 @@ public class CustomHtmlProcessorFactory extends JEditorPaneHtmlMarkupProcessor i
 		return false;
 	}
 	
+	// NOSONAR:START
+	// COPY PASTA FROM JEditorPaneHtmlMarkupProcessor to correct bug 2411
+	public String convert(String srcText)
+	{
+		JEditorPane editorPane = new JEditorPane("text/html", srcText);
+		editorPane.setEditable(false);
+
+		List<Element> elements = new ArrayList<Element>();
+
+		Document document = editorPane.getDocument();
+
+		Element root = document.getDefaultRootElement();
+		if (root != null)
+		{
+			addElements(elements, root);
+		}
+
+		int startOffset = 0;
+		int endOffset = 0;
+		int crtOffset = 0;
+		String chunk = null;
+		JRPrintHyperlink hyperlink = null;
+		Element element = null;
+		Element parent = null;
+		boolean bodyOccurred = false;
+		int[] orderedListIndex = new int[elements.size()];
+		String whitespace = "    ";
+		String[] whitespaces = new String[elements.size()];
+		for(int i = 0; i < elements.size(); i++)
+		{
+			whitespaces[i] = "";
+		}
+		JRStyledText styledText = new JRStyledText();
+		
+		for(int i = 0; i < elements.size(); i++)
+		{
+			if (bodyOccurred && chunk != null)
+			{
+				styledText.append(chunk);
+				Map<Attribute,Object> styleAttributes = getAttributes(element.getAttributes());
+				if (hyperlink != null)
+				{
+					styleAttributes.put(JRTextAttribute.HYPERLINK, hyperlink);
+					hyperlink = null;
+				}
+				if (!styleAttributes.isEmpty())
+				{
+					styledText.addRun(new JRStyledText.Run(styleAttributes, 
+							startOffset + crtOffset, endOffset + crtOffset));
+				}
+			}
+
+			chunk = null;
+			element = elements.get(i);
+			parent = element.getParentElement();
+			startOffset = element.getStartOffset();
+			endOffset = element.getEndOffset();
+			AttributeSet attrs = element.getAttributes();
+
+			Object elementName = attrs.getAttribute(AbstractDocument.ElementNameAttribute);
+			Object object = (elementName != null) ? null : attrs.getAttribute(StyleConstants.NameAttribute);
+			if (object instanceof HTML.Tag) 
+			{
+				
+				HTML.Tag htmlTag = (HTML.Tag) object;
+				if(htmlTag == Tag.BODY)
+				{
+					bodyOccurred = true;
+					crtOffset = - startOffset;
+				}
+				else if(htmlTag == Tag.BR)
+				{
+					chunk = "\n";
+				}
+				else if(htmlTag == Tag.OL)
+				{
+					orderedListIndex[i] = 0;
+					String parentName = parent.getName().toLowerCase();
+					whitespaces[i] = whitespaces[elements.indexOf(parent)] + whitespace;
+					if(parentName.equals("li"))
+					{
+						chunk = "";
+					}
+					else
+					{
+						chunk = "\n";
+						++crtOffset;
+					}
+				}
+				else if(htmlTag == Tag.UL)
+				{
+					whitespaces[i] = whitespaces[elements.indexOf(parent)] + whitespace;
+
+					String parentName = parent.getName().toLowerCase();
+					if(parentName.equals("li"))
+					{
+						chunk = "";
+					}
+					else
+					{
+						chunk = "\n";
+						++crtOffset;
+					}
+					
+				}
+				else if(htmlTag == Tag.LI)
+				{
+					
+					whitespaces[i] = whitespaces[elements.indexOf(parent)];
+					if(element.getElement(0) != null && 
+							(element.getElement(0).getName().toLowerCase().equals("ol") || element.getElement(0).getName().toLowerCase().equals("ul"))
+							)
+					{
+						chunk = "";
+					}
+					else if(parent.getName().equals("ol"))
+					{
+						int index = elements.indexOf(parent);
+						chunk = whitespaces[index] + String.valueOf(++orderedListIndex[index]) + ".  ";
+					} 
+					else
+					{
+						chunk = whitespaces[elements.indexOf(parent)] + "\u2022  ";
+					}
+					crtOffset += chunk.length();
+				}
+				else if (element instanceof LeafElement)
+				{
+					if (element instanceof RunElement)
+					{
+						RunElement runElement = (RunElement)element;
+						AttributeSet attrSet = (AttributeSet)runElement.getAttribute(Tag.A);
+						if (attrSet != null)
+						{
+							hyperlink = new JRBasePrintHyperlink();
+							hyperlink.setHyperlinkType(HyperlinkTypeEnum.REFERENCE);
+							hyperlink.setHyperlinkReference((String)attrSet.getAttribute(HTML.Attribute.HREF));
+							hyperlink.setLinkTarget((String)attrSet.getAttribute(HTML.Attribute.TARGET));
+						}
+					}
+					try
+					{
+						chunk = document.getText(startOffset, endOffset - startOffset);
+					}
+					catch(BadLocationException e)
+					{
+						if (LOGGER.isDebugEnabled())
+						{
+							LOGGER.debug("Error converting markup.", e);
+						}
+					}
+				}
+			}
+		}
+
+		if (chunk != null && !"\n".equals(chunk))
+		{
+			styledText.append(chunk);
+			Map<Attribute,Object> styleAttributes = getAttributes(element.getAttributes());
+			if (hyperlink != null)
+			{
+				styleAttributes.put(JRTextAttribute.HYPERLINK, hyperlink);
+				hyperlink = null;
+			}
+			if (!styleAttributes.isEmpty())
+			{
+				styledText.addRun(new JRStyledText.Run(styleAttributes, 
+						startOffset + crtOffset, endOffset + crtOffset));
+			}
+		}
+		
+		styledText.setGlobalAttributes(new HashMap<Attribute,Object>());
+		// FIX FOR [Issue 2411]
+		List<Run> runs = styledText.getRuns();
+		for(Run run : runs){
+			if (run.endIndex > styledText.length()){
+				run.endIndex = styledText.length();
+			}
+			
+		}
+		// END FIX FOR  [Issue 2411]
+		return JRStyledTextParser.getInstance().write(styledText);
+	}
+	// NOSONAR:END
+	// END COPY PASTA FROM JEditorPaneHtmlMarkupProcessor to correct bug 2411
 }
