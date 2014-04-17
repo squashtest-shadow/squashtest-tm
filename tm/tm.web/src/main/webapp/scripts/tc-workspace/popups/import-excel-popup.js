0b6b626fe6ae53994f259b37179549679ba5ae7f
gfouquet <gfouquet@henix.fr>
2014-04-17 15:54:33 +0200

    Handlebars was not 'required'

--- tm/tm.web/src/main/webapp/scripts/tc-workspace/popups/import-excel-popup.js
+++ tm/tm.web/src/main/webapp/scripts/tc-workspace/popups/import-excel-popup.js
@@ -18,7 +18,7 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define([ "jquery", "tree", "workspace/workspace.import-popup" ], function($, zetree) {
+define([ "jquery", "tree", "handlebars", "workspace/workspace.import-popup" ], function($, zetree, Handlebars) {
 	"use strict";
 
 	var recapBuilder = {};
