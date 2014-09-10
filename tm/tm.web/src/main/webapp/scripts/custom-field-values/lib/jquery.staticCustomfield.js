f75592458efebac2d7455ce5df420a8d3b12c164
bsiri <bsiri@localhost>
2014-09-10 16:19:47 +0200

    [Feat 3823] changing to a specific version for this branch

--- tm/tm.web/src/main/webapp/scripts/custom-field-values/lib/jquery.staticCustomfield.js
+++ tm/tm.web/src/main/webapp/scripts/custom-field-values/lib/jquery.staticCustomfield.js
@@ -139,7 +139,7 @@ define(["jquery", "./cuf-values-utils", "underscore", "squash.configmanager"], f
 		
 		'TAG' : {
 			_build : function(elt, def){
-				var conf = confman
+				var conf = confman;
 				elt.squashTagit(conf);
 				elt.squashTagit('disable');
 			},
