bca29ea6410eec1bff4ac186f78dd8817ea93a61
bsiri <bsiri@localhost>
2014-09-23 16:46:44 +0200

    [Issue 3951] fixed the major break of the ITs

---
+++ integration-tests/src/it/groovy/org/squashtest/it/infrastructure/StubValidatorFactory.java
@@ -0,0 +1,68 @@
+/**
+ *     This file is part of the Squashtest platform.
+ *     Copyright (C) 2010 - 2014 Henix, henix.fr
+ *
+ *     See the NOTICE file distributed with this work for additional
+ *     information regarding copyright ownership.
+ *
+ *     This is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     this software is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.squashtest.it.infrastructure;
+
+import javax.validation.ConstraintValidatorFactory;
+import javax.validation.MessageInterpolator;
+import javax.validation.TraversableResolver;
+import javax.validation.Validator;
+import javax.validation.ValidatorContext;
+import javax.validation.ValidatorFactory;
+
+public class StubValidatorFactory implements ValidatorFactory {
+
+	@Override
+	public Validator getValidator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidatorContext usingContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MessageInterpolator getMessageInterpolator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TraversableResolver getTraversableResolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ConstraintValidatorFactory getConstraintValidatorFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
