5728b20c056a193950f73dca46a1bd3f8c98128b
mfaurel <mfaurel@localhost>
2015-02-26 12:15:17 +0100

    [Issue 4358] Adding datasets with parameters should always work now

--- tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/testcase/parameters/NewDataset.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/testcase/parameters/NewDataset.java
@@ -41,7 +41,8 @@ public class NewDataset extends Dataset {
 		Dataset dataset =  new Dataset();
 		dataset.setName(getName());
 		for(Object[] paramValue : paramValues) {
-			Parameter parameter = parameterFinder.findById((Integer) paramValue[0]);
+			Number intParam = (Number) paramValue[0];
+			Parameter parameter = parameterFinder.findById(intParam.longValue());
 			new DatasetParamValue(parameter, dataset, (String) paramValue[1]);
 		}
 		return dataset;
