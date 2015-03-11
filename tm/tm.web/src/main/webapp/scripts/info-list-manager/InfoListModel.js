74c21672efe37f41e79f6bb76fa75346c58c10b8
gfouquet <gfouquet@henix.fr>
2015-03-11 16:20:35 +0100

    Fixed : `null` module breaks rjs optimization on ci server

--- tm/tm.web/src/main/webapp/scripts/info-list-manager/InfoListModel.js
+++ tm/tm.web/src/main/webapp/scripts/info-list-manager/InfoListModel.js
@@ -18,7 +18,7 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define([ "backbone", "underscore", "squash.translator", , "../app/squash.backbone.validation" ], function(Backbone, _,
+define([ "backbone", "underscore", "squash.translator", "../app/squash.backbone.validation" ], function(Backbone, _,
 		messages) {
 	"use strict";
 	messages.load([ "message.noDefaultOption" ]);
