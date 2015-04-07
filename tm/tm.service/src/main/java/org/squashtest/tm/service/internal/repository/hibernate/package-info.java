a3129a5082ed3a74c8d0b2ce48042abc81233a00
bsiri <bsiri@localhost>
2015-04-07 16:10:47 +0200

    [Issue 4689] fixed

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/hibernate/package-info.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/hibernate/package-info.java
@@ -752,9 +752,9 @@
 	@NamedQuery(name="infoList.setReqCatToDefault",  query="update RequirementVersion req set req.category = :default  where req.category.id in (select infoItem.id from InfoListItem infoItem inner join infoItem.infoList infoList where infoList.id = :id)"),
 	@NamedQuery(name="infoList.setTcNatToDefault",  query="update TestCase tc set tc.nature = :default where tc.nature.id in (select infoItem.id from InfoListItem infoItem inner join infoItem.infoList infoList where infoList.id = :id)"),
 	@NamedQuery(name="infoList.setTcTypeToDefault",  query="update TestCase tc set tc.type = :default where tc.type.id in (select infoItem.id from InfoListItem infoItem inner join infoItem.infoList infoList where infoList.id = :id)"),
-	@NamedQuery(name="infoList.project.setReqCatListToDefault",  query="update Project p set p.requirementCategories = :default where p.requirementCategories.id = :id"),
-	@NamedQuery(name="infoList.project.setTcNatListToDefault",  query="update Project p set p.testCaseNatures = :default where p.testCaseNatures.id = :id"),
-	@NamedQuery(name="infoList.project.setTcTypeListToDefault",  query="update Project p set p.testCaseTypes = :default where p.testCaseTypes.id = :id"),
+	@NamedQuery(name="infoList.project.setReqCatListToDefault",  query="update GenericProject p set p.requirementCategories = :default where p.requirementCategories.id = :id"),
+	@NamedQuery(name="infoList.project.setTcNatListToDefault",  query="update GenericProject p set p.testCaseNatures = :default where p.testCaseNatures.id = :id"),
+	@NamedQuery(name="infoList.project.setTcTypeListToDefault",  query="update GenericProject p set p.testCaseTypes = :default where p.testCaseTypes.id = :id"),
 
 	//InfoListItem deletion
 	@NamedQuery(name="infoListItem.setReqCatToDefault", query="update RequirementVersion req set req.category = :default  where req.category.id = :id"),
