c0f0325f76f96558dca9479490ffc292ff1dd8de
gfouquet <gfouquet@henix.fr>
2015-04-15 17:23:40 +0200

    Dataset were not insertable in a case-sensitive mysql (i.e. not on windows box)

--- database/src/main/liquibase/sample/sample-db-data-1.3.0.sql
+++ database/src/main/liquibase/sample/sample-db-data-1.3.0.sql
@@ -1,8 +1,8 @@
-Insert into core_user(ID, LOGIN, FIRST_NAME, LAST_NAME, EMAIL, ACTIVE, CREATED_BY, CREATED_ON, LAST_MODIFIED_ON, LAST_MODIFIED_BY) values 
+Insert into CORE_USER(ID, LOGIN, FIRST_NAME, LAST_NAME, EMAIL, ACTIVE, CREATED_BY, CREATED_ON, LAST_MODIFIED_ON, LAST_MODIFIED_BY) values 
  (33, 'taserver', 'TA', 'Server', 'no@where', true, 'admin', '2011-09-30 10:26:23.0', null, null);
-Insert into auth_user(LOGIN, PASSWORD, ACTIVE) values 
+Insert into AUTH_USER(LOGIN, PASSWORD, ACTIVE) values 
  ('taserver', 'eb0fd24715f33bc94b1754263d1d431422aba649', true); 
-Insert into core_group_member(USER_ID, GROUP_ID) values 
+Insert into CORE_GROUP_MEMBER(USER_ID, GROUP_ID) values 
  (33, 4);
-Update test_case
+Update TEST_CASE
 set PREREQUISITE='';
