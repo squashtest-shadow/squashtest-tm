f7c64be7a6d739216957f36a9db1bf82cbad6d64
gfouquet <gfouquet@henix.fr>
2015-04-15 11:07:20 +0200

    [Issue 4389] Togglepanels can now be marked as "frozen" to prevent toggling

--- tm/tm.web/src/main/webapp/scripts/squashtest/jquery.squash.togglepanels.js
+++ tm/tm.web/src/main/webapp/scripts/squashtest/jquery.squash.togglepanels.js
@@ -19,8 +19,13 @@
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 (function($) {
-	// the über-new toggle-panel: each .sq-tg is automatically turned into a toggle panel
-	$(document).on("click", ".sq-tg .tg-head", function(event) {
+	/**
+	 * The über-new toggle-panel: each `.sq-tg` is automatically turned into a toggle panel
+	 *
+	 * A toggle panel (`.sq-tg`) can be marked as `.frozen`. In that case, it no longer (un)folds
+	 * Remove the `.frozen` class to let it go
+	 */
+	$(document).on("click", ".sq-tg:not(.frozen) .tg-head", function(event) {
 		var $target = $(event.target);
 
 		if ($target.parents(".tg-toolbar").length > 0) {
@@ -31,14 +36,13 @@
 		event.stopImmediatePropagation();
 
 		var $panel = $target.parents(".sq-tg");
-	
+
 		$panel.find(".tg-body").toggle("blind", 500, function() {
 			$panel.toggleClass("collapse");
 			$panel.toggleClass("expand");
 
 			$panel.find(".tg-toolbar .sq-btn, button, input, a").prop("disabled", $panel.is(".collapse"));
 		});
-
 	});
 
 	// turns .sq-tl into toggle lists
