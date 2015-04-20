35bbea8343f4947fa495f9a718b6da0a18f9ed8c
gfouquet <gfouquet@henix.fr>
2015-04-20 12:00:45 +0200

    [Issue 4662] Prevent export of anything but selected campaign Factored out code using higher order function

--- tm/tm.web/src/main/webapp/scripts/camp-workspace/init-actions.js
+++ tm/tm.web/src/main/webapp/scripts/camp-workspace/init-actions.js
@@ -18,12 +18,12 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(["jquery", "tree","./permissions-rules", "workspace.contextual-content", "workspace.event-bus", "squash.translator" , 
+define(["jquery", "tree","./permissions-rules", "workspace.contextual-content", "workspace.event-bus", "squash.translator" ,
         "workspace.tree-node-copier", "workspace.tree-event-handler"], function($, zetree, rules, ctxcontent, eventBus,  translator, copier, treehandler){
-	
-	
-	function showError(messageName){	
-		
+	"use strict";
+
+	function showError(messageName){
+
 		var messages = translator.get({
 			"no-libraries-allowed"	: "tree.button.copy-node.error.nolibrary",
 			"not-unique"			: "tree.button.copy-node.error.notOneEditable",
@@ -34,11 +34,11 @@ define(["jquery", "tree","./permissions-rules", "workspace.contextual-content", 
 			"mixed-nodes-iteration-selection" : "tree.button.copy-node.mixediteration",
 			"mixed-nodes-testsuite-selection" : "tree.button.copy-node.mixedsuite",
 			"milestone-denied"		: "squashtm.action.exception.milestonelocked"
-		});		
+		});
 
 		squashtm.notification.showInfo(messages[messageName]);
 	}
-	
+
 
 	function copyIfOk(tree){
 		var nodes = tree.jstree("get_selected");
@@ -48,9 +48,9 @@ define(["jquery", "tree","./permissions-rules", "workspace.contextual-content", 
 		else{
 			var why = rules.whyCantCopy(nodes);
 			showError(why);
-		}		
+		}
 	}
-	
+
 	function pasteIfOk(tree){
 		if (rules.canPaste()){
 			copier.pasteNodesFromCookie();
@@ -58,49 +58,72 @@ define(["jquery", "tree","./permissions-rules", "workspace.contextual-content", 
 		else{
 			var why = rules.whyCantPaste();
 			showError(why);
-		}		
+		}
 	}
-	
+
 	function loadFragment(tree){
 		var selected =  tree.jstree("get_selected");
 		if (selected.length == 1){
 			ctxcontent.loadWith(selected.getResourceUrl());
 		}
 		else{
-			ctxcontent.unload();				
+			ctxcontent.unload();
 		}
 	}
-	
+
+	function onClickExport(tree, exporter) {
+		return function(event) {
+			var nodeId = tree.jstree("get_selected").attr("resid");
+			var nodeType = tree.jstree("get_selected").attr("restype");
+
+			if (nodeId === undefined || nodeType !== "campaigns") {
+				var dialog = $("#export-campaign-error-dialog").formDialog();
+				dialog.on('formdialogcancel', function(){
+					dialog.formDialog('close');
+				});
+				dialog.formDialog('open');
+
+			} else {
+				exporter.call(this, {source: event, nodeType: nodeType, nodeId: nodeId});
+			}
+
+		};
+	}
+
+	function exportUrl(nodeId) {
+		return squashtm.app.contextRoot+"/campaign-browser/export-campaign/" + nodeId;
+	}
+
 	return {
 		init : function(){
-			
+
 			var tree = zetree.get();
-			
+
 			tree.on("select_node.jstree deselect_node.jstree", function(){
 				loadFragment(tree);
 			});
-			
+
 			// ************* creation ***************
-			
+
 			$("#new-folder-tree-button").on("click", function(){
 				$("#add-folder-dialog").formDialog("open");
 			});
-			
+
 			$("#new-campaign-tree-button").on("click", function(){
 				$("#add-campaign-dialog").formDialog("open");
 			});
-			
+
 			$("#new-iteration-tree-button").on("click", function(){
 				$("#add-iteration-dialog").formDialog("open");
 			});
-						
+
 			// *************** copy paste ****************
-			
+
 			$("#copy-node-tree-button").on("click", function(){
 				copyIfOk(tree);
 			});
-			
-			// issue 2762 : the events "copy.squashtree" and the native js event "copy" (also triggered using ctrl+c) would both fire this 
+
+			// issue 2762 : the events "copy.squashtree" and the native js event "copy" (also triggered using ctrl+c) would both fire this
 			// handler. Its a bug of jquery, fixed in 1.9.
 			// TODO : upgrade to jquery 1.9
 			tree.on("copy.squashtree", function(evt){
@@ -108,94 +131,55 @@ define(["jquery", "tree","./permissions-rules", "workspace.contextual-content", 
 					copyIfOk(tree);
 				}
 			});
-			
+
 			$("#paste-node-tree-button").on("click", function(){
 				pasteIfOk(tree);
-			});			
-			
-			// issue 2762 : the events "paste.squashtree" and the native js event "paste" (also triggered using ctrl+v) would both fire this 
+			});
+
+			// issue 2762 : the events "paste.squashtree" and the native js event "paste" (also triggered using ctrl+v) would both fire this
 			// handler. Its a bug of jquery, fixed in 1.9
 			// TODO : upgrade to jquery 1.9
 			tree.on("paste.squashtree", function(evt){
 				if (evt.namespace === "squashtree"){
-					pasteIfOk(tree);					
+					pasteIfOk(tree);
 				}
 			});
-			
+
 			// ***************** rename **********************
-			
+
 			$("#rename-node-tree-button").on("click", function(){
 				$("#rename-node-dialog").formDialog("open");
 			});
-			
+
 			tree.on("rename.squashtree", function(){
 				$("#rename-node-dialog").formDialog("open");
 			});
-			
-			
+
 			// ****************** exports *********************
-			
+
 			// NOTE : DO NOT BIND USING $("menu").on("click", "button", handler), this breaks under (true) IE8. See #3268
-			$("#export-L-tree-button").on("click", function(){
-				
-				var dialog = $("#export-campaign-error-dialog").formDialog();
-				dialog.on('formdialogcancel', function(){
-					dialog.formDialog('close');
-				});
-				var nodeId = tree.jstree("get_selected").attr("resid");		
-				var nodeType = tree.jstree("get_selected").attr("restype");	
-				
-				if (nodeId === undefined || nodeType == "campaign-libraries") {
-					dialog.formDialog('open');
-				}
-				else {
-				document.location.href= squashtm.app.contextRoot+"/campaign-browser/export-campaign/"+nodeId+"?export=csv&exportType=L";
-				}
-			});
-			
-			$("#export-S-tree-button").on("click", function(){				
-				var dialog = $("#export-campaign-error-dialog").formDialog();
-				dialog.on('formdialogcancel', function(){
-					dialog.formDialog('close');
-				});
-				var nodeId = tree.jstree("get_selected").attr("resid");		
-				var nodeType = tree.jstree("get_selected").attr("restype");	
-				
-				if (nodeId === undefined || nodeType == "campaign-libraries") {
-					dialog.formDialog('open');
-				}
-				else {
-					document.location.href= squashtm.app.contextRoot+"/campaign-browser/export-campaign/"+nodeId+"?export=csv&exportType=S";
-				}
-			}); 
-			
-			$("#export-F-tree-button").on("click", function(){				
-				var dialog = $("#export-campaign-error-dialog").formDialog();
-				dialog.on('formdialogcancel', function(){
-					dialog.formDialog('close');
-				});
-				var nodeId = tree.jstree("get_selected").attr("resid");		
-				var nodeType = tree.jstree("get_selected").attr("restype");	
-				
-				if (nodeId === undefined || nodeType == "campaign-libraries") {
-					dialog.formDialog('open');
-				}
-				else {
-				document.location.href=  squashtm.app.contextRoot+"/campaign-browser/export-campaign/"+nodeId+"?export=csv&exportType=F";
-				}
-			});		
-			
-			
+			$("#export-L-tree-button").on("click", onClickExport(tree, function(event){
+				document.location.href = exportUrl(event.nodeId) + "?export=csv&exportType=L";
+			}));
+
+			$("#export-S-tree-button").on("click", onClickExport(tree, function(event){
+					document.location.href= exportUrl(event.nodeId) + "?export=csv&exportType=S";
+			}));
+
+			$("#export-F-tree-button").on("click", onClickExport(tree, function(event){
+				document.location.href = exportUrl(event.nodeId) + "?export=csv&exportType=F";
+			}));
+
 			// ***************** deletion ********************
-			
+
 			$("#delete-node-tree-button").on("click", function(){
 				$("#delete-node-dialog").delcampDialog("open");
 			});
-			
+
 			tree.on("suppr.squashtree", function(){
 				$("#delete-node-dialog").delcampDialog("open");
 			});
 		}
-	};	
-	
+	};
+
 });
