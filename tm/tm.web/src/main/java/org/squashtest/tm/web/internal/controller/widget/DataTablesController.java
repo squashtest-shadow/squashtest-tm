1190a8aeae14bc16cf451761e5a47bce82ad34cd
gfouquet <gfouquet@henix.fr>
2015-04-21 18:02:02 +0200

    [Issue 4471] Alternate message when list is bound. BSR removed useless js config in html template

--- tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/widget/DataTablesController.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/internal/controller/widget/DataTablesController.java
@@ -33,32 +33,76 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Gregory Fouquet
- *
+ * 
  */
 @Controller
 public class DataTablesController {
 	@Inject
 	private MessageSource messageSource;
 
+	private Map<String, Object> legacyMessages;
+	private Map<String, Object> messages;
+
+	/**
+	 * Internationalized messages for Datatable < 1.10
+	 * 
+	 * @param locale
+	 * @return
+	 */
 	@RequestMapping("/datatables/messages")
 	@ResponseBody
 	public Map<String, Object> getInternationalizedMessages(Locale locale) {
-		Map<String, Object> messages = new HashMap<String, Object>();
+		if (legacyMessages == null) {
+			legacyMessages = new HashMap<String, Object>();
 
-		messages.put("sLengthMenu", messageSource.getMessage("generics.datatable.lengthMenu", null, locale));
-		messages.put("sZeroRecords", messageSource.getMessage("generics.datatable.zeroRecords", null, locale));
-		messages.put("sInfo", messageSource.getMessage("generics.datatable.info", null, locale));
-		messages.put("sInfoEmpty", messageSource.getMessage("generics.datatable.infoEmpty", null, locale));
-		messages.put("sInfoFiltered", messageSource.getMessage("generics.datatable.infoFiltered", null, locale));
-		messages.put("sSearch", messageSource.getMessage("generics.datatable.search", null, locale));
+			legacyMessages.put("sLengthMenu", messageSource.getMessage("generics.datatable.lengthMenu", null, locale));
+			legacyMessages
+			.put("sZeroRecords", messageSource.getMessage("generics.datatable.zeroRecords", null, locale));
+			legacyMessages.put("sInfo", messageSource.getMessage("generics.datatable.info", null, locale));
+			legacyMessages.put("sInfoEmpty", messageSource.getMessage("generics.datatable.infoEmpty", null, locale));
+			legacyMessages.put("sInfoFiltered",
+					messageSource.getMessage("generics.datatable.infoFiltered", null, locale));
+			legacyMessages.put("sSearch", messageSource.getMessage("generics.datatable.search", null, locale));
 
-		Map<String, Object> pagination = new HashMap<String, Object>();
-		messages.put("oPaginate", pagination);
+			Map<String, Object> pagination = new HashMap<String, Object>();
+			legacyMessages.put("oPaginate", pagination);
 
-		pagination.put("sFirst", messageSource.getMessage("generics.datatable.paginate.first", null, locale));
-		pagination.put("sPrevious", messageSource.getMessage("generics.datatable.paginate.previous", null, locale));
-		pagination.put("sNext", messageSource.getMessage("generics.datatable.paginate.next", null, locale));
-		pagination.put("sLast", messageSource.getMessage("generics.datatable.paginate.last", null, locale));
+			pagination.put("sFirst", messageSource.getMessage("generics.datatable.paginate.first", null, locale));
+			pagination.put("sPrevious", messageSource.getMessage("generics.datatable.paginate.previous", null, locale));
+			pagination.put("sNext", messageSource.getMessage("generics.datatable.paginate.next", null, locale));
+			pagination.put("sLast", messageSource.getMessage("generics.datatable.paginate.last", null, locale));
+		}
+
+		return legacyMessages;
+	}
+
+	/**
+	 * Internationalized messages for Datatable >= 1.10
+	 * 
+	 * @param locale
+	 * @return
+	 */
+	@RequestMapping("/datatables/language")
+	@ResponseBody
+	public Map<String, Object> getLanguage(Locale locale) {
+		if (messages == null) {
+			messages = new HashMap<String, Object>();
+
+			messages.put("search", messageSource.getMessage("generics.datatable.search", null, locale));
+			messages.put("lengthMenu", messageSource.getMessage("generics.datatable.lengthMenu", null, locale));
+			messages.put("info", messageSource.getMessage("generics.datatable.info", null, locale));
+			messages.put("infoEmpty", messageSource.getMessage("generics.datatable.infoEmpty", null, locale));
+			messages.put("infoFiltered", messageSource.getMessage("generics.datatable.infoFiltered", null, locale));
+			messages.put("zeroRecords", messageSource.getMessage("generics.datatable.zeroRecords", null, locale));
+
+			Map<String, Object> pagination = new HashMap<String, Object>();
+			messages.put("paginate", pagination);
+
+			pagination.put("first", messageSource.getMessage("generics.datatable.paginate.first", null, locale));
+			pagination.put("previous", messageSource.getMessage("generics.datatable.paginate.previous", null, locale));
+			pagination.put("next", messageSource.getMessage("generics.datatable.paginate.next", null, locale));
+			pagination.put("last", messageSource.getMessage("generics.datatable.paginate.last", null, locale));
+		}
 
 		return messages;
 	}
--- tm/tm.web/src/main/webapp/scripts/info-list-manager/InfoListsTable.js
+++ tm/tm.web/src/main/webapp/scripts/info-list-manager/InfoListsTable.js
@@ -19,313 +19,289 @@
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 define(
-		[ "jquery", "backbone", "underscore", "../squashtable/squashtable.options", "jquery.squash.oneshotdialog", "squash.translator", "app/ws/squashtm.notification"],
-		function($, Backbone, _, SquashTable, oneshot, messages, notif) {
-			"use strict";
+	[ "jquery", "backbone", "underscore", "../squashtable/squashtable.options", "jquery.squash.oneshotdialog", "squash.translator", "app/ws/squashtm.notification"],
+	function($, Backbone, _, SquashTable, oneshot, messages, notif) {
+		"use strict";
 
-			messages.load([
-				"message.infoList.remove.first",
-				"message.infoList.remove.second",
-				"message.infoList.remove.third",
-				"message.infoList.remove.fourth",
-				"message.infoList.bound.remove.first",
-				"message.infoList.bound.remove.second",
-				"message.infoList.bound.remove.third",
-				"message.infoList.bound.remove.fourth",
-				"message.infoList.batchRemove.first",
-				"message.infoList.batchRemove.second",
-				"message.infoList.batchRemove.third",
-				"message.infoList.batchRemove.fourth",
-				"message.infoList.batchRemove.first",
-				"message.infoList.bound.batchRemove.second",
-				"message.infoList.bound.batchRemove.third",
-				"message.infoList.bound.batchRemove.fourth",
-				"message.noListSelected",
-				"dialog.info-list.warning.reindex.before",
-				"dialog.info-list.warning.reindex.after"
-			]);
-			
-			var oLanguage = messages.get({
-				'sLengthMenu' : 'generics.datatable.lengthMenu',
-				'sZeroRecords' : 'generics.datatable.zeroRecords',
-				'sInfo' : 'generics.datatable.info',
-				'sInfoEmpty' : 'generics.datatable.infoEmpty',
-				'sInfoFiltered' : 'generics.datatable.infoFiltered',
-				'sSearch' : 'generics.datatable.search',
-				'oPaginage' : {
-					'sFirst' : 'generics.datatable.paginate.first',
-					'sPrevious' : 'generics.datatable.paginate.previous',
-					'sNext' : 'generics.datatable.paginate.next',
-					'sLast' : 'generics.datatable.paginate.last'
+		messages.load([
+			"message.infoList.remove.first",
+			"message.infoList.remove.second",
+			"message.infoList.remove.third",
+			"message.infoList.remove.fourth",
+			"message.infoList.bound.remove.first",
+			"message.infoList.bound.remove.second",
+			"message.infoList.bound.remove.third",
+			"message.infoList.bound.remove.fourth",
+			"message.infoList.batchRemove.first",
+			"message.infoList.batchRemove.second",
+			"message.infoList.batchRemove.third",
+			"message.infoList.batchRemove.fourth",
+			"message.infoList.batchRemove.first",
+			"message.infoList.bound.batchRemove.second",
+			"message.infoList.bound.batchRemove.third",
+			"message.infoList.bound.batchRemove.fourth",
+			"message.noListSelected",
+			"dialog.info-list.warning.reindex.before",
+			"label.GotoIndex"
+		]);
+
+		var itemsTableConf = window.squashtm.app.itemsTable;
+		console.log("table conf" ,itemsTableConf);
+
+		function buggerOffReindex(event) {
+			$(this).dialog("close").dialog("destroy");
+			this.remove();
+		}
+
+		var gotoIndexButton = {
+				'text' : messages.get("label.GotoIndex"),
+				'click' : function() {
+					buggerOffReindex.apply(this, arguments);
+					document.location.href=  squashtm.app.contextRoot + "/administration/indexes";
 				}
-			});
-			
-			var itemsTableConf = window.squashtm.app.itemsTable;
-			console.log("table conf" ,itemsTableConf);
-			
-			var gotoIndexButton = {
-					'text' : messages.get("label.GotoIndex"),
-					'click' : function() {
-						var jqDialog = $(this);
-						jqDialog.dialog('close');
-						jqDialog.dialog('destroy');
-						this.remove();
-						document.location.href=  squashtm.app.contextRoot + "/administration/indexes";
-					}
-				};
-			
-			var closeButton = {
-					'text' : messages.get("label.Close"),
-					'click' : function() {
-						var jqDialog = $(this);
-						jqDialog.dialog('close');
-						jqDialog.dialog('destroy');
-						this.remove();
-					}
-				};
-			
-			var isAdmin = window.squashtm.app.isAdmin;
-			
-			function selectTr($tr) {
-				$tr.removeClass("ui-state-highlight").addClass("ui-state-row-selected");
-			}
+			};
 
-			function itemIdMapper(data) {
-				return data.id;
-			}
+		var closeButton = {
+				'text' : messages.get("label.Close"),
+				'click' : buggerOffReindex
+			};
 
-			function removeTemplate() {
-				removeTemplate.tpl = removeTemplate.tpl || Handlebars.compile($("#confirm-remove-tpl").html());
-				return removeTemplate.tpl;
-			}
-			
-			function reindexTemplate(){
-				reindexTemplate.tpl = reindexTemplate.tpl || Handlebars.compile($("#confirm-remove-reindex").html())({ 'warn-index': messages.get("dialog.info-list.warning.reindex.after")});
-				return reindexTemplate.tpl;
-				
-			}
-			
-			function popupReindex(){
-                var indxTpl = reindexTemplate();
-				var buttonConf = [ ];
-				if (isAdmin){
-				buttonConf.push(gotoIndexButton);
-				}
-				buttonConf.push(closeButton);
-				var conf =  {'buttons' : buttonConf};
-				oneshot.show(messages.get("label.Delete"), indxTpl, conf);
-			}
+		var isAdmin = window.squashtm.app.isAdmin;
 
-			function removeProps(batch) {
-				var flavor = batch ? "batchRemove." : "remove.";
+		function selectTr($tr) {
+			$tr.removeClass("ui-state-highlight").addClass("ui-state-row-selected");
+		}
 
-				return function(bound) {
-					var binding = bound ? "bound." : "";
-					var memo = bound ? {'warn-index':  messages.get("dialog.info-list.warning.reindex.before")} : {'warn-index': ""};
+		function itemIdMapper(data) {
+			return data.id;
+		}
 
-					return ["first", "second", "third", "fourth"].reduce(function(memo, item) {
-						memo[item] = messages.get("message.infoList." + binding + flavor + item);
-						return memo;
-					}, memo);
-				};
-			}
+		function removeTemplate() {
+			removeTemplate.tpl = removeTemplate.tpl || Handlebars.compile($("#confirm-remove-tpl").html());
+			return removeTemplate.tpl;
+		}
 
-			/*
-			 * Defines the controller for the custom fields table.
-			 */
-			var View = Backbone.View.extend({
-				el : "#items-table",
-				initialize : function() {
-					var self = this;
+		function reindexTemplate() {
+			reindexTemplate.tpl = reindexTemplate.tpl || Handlebars.compile($("#confirm-remove-reindex").html());
+			return reindexTemplate.tpl;
+		}
 
-					_.bindAll(this, "refresh", "onInitTable", "removeSelectedItems");
+		function popupReindex() {
+			var buttonConf = [];
+			isAdmin && buttonConf.push(gotoIndexButton);
+			buttonConf.push(closeButton);
+			oneshot.show(messages.get("label.Delete"), reindexTemplate()(), { buttons : buttonConf });
+		}
 
-					this.apiRoot = this.$el.data("api-url");
-					this.viewUrl = this.$el.data("view-url");
-					this.selectedIds = [];
+		function removeProps(batch) {
+			var flavor = batch ? "batchRemove." : "remove.";
 
-					this.listenTo(squashtm.vent, "newinfolist:confirmed", function(event) {
-						self.refresh();
-					});
+			return function(bound) {
+				var binding = bound ? "bound." : "";
+				var memo = {"warn-index":  (bound ? messages.get("dialog.info-list.warning.reindex.before") : "") };
 
-					this.$el.on("init.dt", function(event) { self.onInitTable(event); });
+				return ["first", "second", "third", "fourth"].reduce(function(memo, item) {
+					memo[item] = messages.get("message.infoList." + binding + flavor + item);
+					return memo;
+				}, memo);
+			};
+		}
 
-					var remove = SquashTable.renderer($("#remove-cell-tpl").html())(function(data, type, row) {
-						return { value: row.id, name: "list-delete" };
-					});
+		/*
+		 * Defines the controller for the custom fields table.
+		 */
+		var View = Backbone.View.extend({
+			el : "#items-table",
+			initialize : function() {
+				var self = this;
 
-					var editLink = SquashTable.renderer($("#name-cell-tpl").html())(function(data, type, row) {
-						return { url:  self.viewUrl + "/" + row.id, text: data };
-					});
+				_.bindAll(this, "refresh", "onInitTable", "removeSelectedItems");
 
-					var colDefs = SquashTable.colDefs()
-						.hidden(0, "id")
-						.index(1)
-						.std({ targets: 2, data: "label", render: editLink })
-						.std(3, "description")
-						.std(4, "defaultLabel")
-						.std(5, "code")
-						.calendar(6, "createdOn")
-						.std(7, "createdBy")
-						.datetime(8, "lastModifiedOn")
-						.std(9, "lastModifiedBy")
-						.button({ targets: 10, render: remove })
-						.hidden(11, "bound")
-						.build();
+				this.apiRoot = this.$el.data("api-url");
+				this.viewUrl = this.$el.data("view-url");
+				this.selectedIds = [];
 
-					var tableConfig = {
-							jQueryUI: true,
-							searching: true,
-							pagingType: "squash",
-							pageLength: itemsTableConf.pageLength,
-							dom: '<"dataTables_header"fr>t<"dataTables_footer"lp>',
-							order: [ [ 2, "asc" ] ],
-							columnDefs: colDefs,
-							"language" : oLanguage,
-							// we cannot init ajax with deferred fetch and client-side processing,
-							// so ajax is configured later on "init.dt" event
+				this.listenTo(squashtm.vent, "newinfolist:confirmed", function(event) {
+					self.refresh();
+				});
+
+				this.$el.on("init.dt", function(event) { self.onInitTable(event); });
+
+				var remove = SquashTable.renderer($("#remove-cell-tpl").html())(function(data, type, row) {
+					return { value: row.id, name: "list-delete" };
+				});
+
+				var editLink = SquashTable.renderer($("#name-cell-tpl").html())(function(data, type, row) {
+					return { url:  self.viewUrl + "/" + row.id, text: data };
+				});
+
+				var colDefs = SquashTable.colDefs()
+					.hidden(0, "id")
+					.index(1)
+					.std({ targets: 2, data: "label", render: editLink })
+					.std(3, "description")
+					.std(4, "defaultLabel")
+					.std(5, "code")
+					.calendar(6, "createdOn")
+					.std(7, "createdBy")
+					.datetime(8, "lastModifiedOn")
+					.std(9, "lastModifiedBy")
+					.button({ targets: 10, render: remove })
+					.hidden(11, "bound")
+					.build();
+
+				var tableConfig = {
+						jQueryUI: true,
+						searching: true,
+						pagingType: "squash",
+						pageLength: this.$el.data("pageLength"),
+						dom: '<"dataTables_header"fr>t<"dataTables_footer"lp>',
+						order: [ [ 2, "asc" ] ],
+						columnDefs: colDefs,
+						language : {
+							url: this.$el.data("language-url")
+						},
+						// we cannot init ajax with deferred fetch and client-side processing,
+						// so ajax is configured later on "init.dt" event
 //							ajax: this.viewUrl,
-							deferLoading: this.$("tbody > tr").size()
-					};
+						deferLoading: this.$("tbody > tr").size()
+				};
 
-					this.$el.DataTable(tableConfig);
-				},
+				this.$el.DataTable(tableConfig);
+			},
 
-				events: {
-					"click button[name='list-delete']": "onClickListDelete",
-					"click td.select-handle": "onClickSelectHandle",
-					"draw.dt": "onDrawTable",
-					"order.dt" : "fixorder",
-					"search.dt" : "fixorder",
-				},
+			events: {
+				"click button[name='list-delete']": "onClickListDelete",
+				"click td.select-handle": "onClickSelectHandle",
+				"draw.dt": "onDrawTable",
+				"order.dt" : "fixorder",
+				"search.dt" : "fixorder",
+			},
 
-				fixorder : function(){
-					//fix order
-					var self = this;
-				    
-					 $.each(self.$el.dataTable().fnGetNodes(), function(index, cell){
-						 cell._DT_RowIndex= index;
-						 cell.firstElementChild.innerHTML = index + 1;
-					 });
-				    
-				    
-				},
-				onInitTable: function onInitTable(event) {
-					this.$el.DataTable().ajax.url(this.viewUrl);
-				},
+			fixorder : function(){
+				//fix order
+				var self = this;
 
-				onDrawTable: function(event) {
-					var self = this;
+				 $.each(self.$el.dataTable().fnGetNodes(), function(index, cell){
+					 cell._DT_RowIndex= index;
+					 cell.firstElementChild.innerHTML = index + 1;
+				 });
 
-					var selector = function(idx, data, node) {
-						return self.selectedIds.indexOf(data.id) > -1;
-					};
 
-					var $trs = self.$el.DataTable().rows(selector).nodes().to$();
-					selectTr($trs);
-				},
+			},
 
-				/**
-				 * refreshes this view / refetches table content. Context is bound to this object.
-				 * @param event
-				 */
-				refresh: function() {
-					this.$el.DataTable().ajax.reload();
-				},
+			onInitTable: function onInitTable(event) {
+				this.$el.DataTable().ajax.url(this.viewUrl);
+			},
 
-				onClickSelectHandle: function(event) {
-					// had to copy from function buried inside squashtable-main.
-					// this is sort of generic code, it should be factored out somewhere reachable
-					var c = event.ctrlKey;
-					var s = event.shiftKey;
-					var $tr = $(event.currentTarget).closest("tr");
-					var self = this;
+			onDrawTable: function(event) {
+				var self = this;
 
-					var toggleSelection = function($tr) {
-						var $trs = self.$("tr");
-						$trs.removeClass("ui-state-row-selected");
-						self.selectedIds = [];
-						addToSelection($tr);
-					};
+				var selector = function(idx, data, node) {
+					return self.selectedIds.indexOf(data.id) > -1;
+				};
 
-					var addToSelection = function($tr) {
-						selectTr($tr);
-						var row = self.$el.DataTable().row($tr);
-						self.lastSelectedRow = row.index();
-						self.selectedIds.push(row.data().id);
-					};
+				var $trs = self.$el.DataTable().rows(selector).nodes().to$();
+				selectTr($trs);
+			},
 
-					var growSelection = function($tr) {
-						var range = computeSelectionRange($tr);
-						self.$el.DataTable().rows(range).nodes().each(function(row) {
-							addToSelection($(row));
-						});
-					};
+			/**
+			 * refreshes this view / refetches table content. Context is bound to this object.
+			 * @param event
+			 */
+			refresh: function() {
+				this.$el.DataTable().ajax.reload();
+			},
 
-					var computeSelectionRange = function($tr) {
-						var base = self.lastSelectedRow || 0;
-						var current = self.$el.DataTable().row($tr).index();
+			onClickSelectHandle: function(event) {
+				// had to copy from function buried inside squashtable-main.
+				// this is sort of generic code, it should be factored out somewhere reachable
+				var c = event.ctrlKey;
+				var s = event.shiftKey;
+				var $tr = $(event.currentTarget).closest("tr");
+				var self = this;
 
-						var min = Math.min(base, current);
-						min = Math.max(min, 0);
+				var toggleSelection = function($tr) {
+					var $trs = self.$("tr");
+					$trs.removeClass("ui-state-row-selected");
+					self.selectedIds = [];
+					addToSelection($tr);
+				};
 
-						var max = Math.max(base, current);
-						max = Math.min(max, self.$("tr").length - 1);
+				var addToSelection = function($tr) {
+					selectTr($tr);
+					var row = self.$el.DataTable().row($tr);
+					self.lastSelectedRow = row.index();
+					self.selectedIds.push(row.data().id);
+				};
 
-						return _.range(min, max + 1);
-					};
-
-					if (!c && !s) {
-						toggleSelection($tr);
-					} else if (c & !s) {
-						addToSelection($tr);
-					} else if (!c & s) {
-						growSelection($tr);
-					} else {
-						growSelection($tr);
-					}
-				},
-
-				onClickListDelete: function onClickListDelete(event) {
-					var self = this;
-					var tgt =  event.currentTarget;
-					var $tr = $(tgt).closest("tr");
-					var isBound = this.$el.DataTable().row($tr).data().bound === true;
-					var props = removeProps(false /* not batch */);
-					var tpl = removeTemplate()(props(isBound));
-					oneshot.show(messages.get("label.Delete"), tpl).done(function() {
-						if (isBound){
-						popupReindex();
-						}						
-						$.ajax(self.apiRoot + "/" + $(tgt).data("value"), { type: "DELETE" })
-							.done(self.refresh);
+				var growSelection = function($tr) {
+					var range = computeSelectionRange($tr);
+					self.$el.DataTable().rows(range).nodes().each(function(row) {
+						addToSelection($(row));
 					});
-				},
+				};
 
-				removeSelectedItems: function removeSelectedItems() {
-					var self = this;
-					var $sel = this.$(".ui-state-row-selected");
-					var rows = this.$el.DataTable().rows($sel);
+				var computeSelectionRange = function($tr) {
+					var base = self.lastSelectedRow || 0;
+					var current = self.$el.DataTable().row($tr).index();
 
-					if (rows.data().length === 0) {
-						notif.showWarning(messages.get("message.noListSelected"));
-						return;
-					}
+					var min = Math.min(base, current);
+					min = Math.max(min, 0);
 
-					var hasBound = _.some(rows.data(), function(data) { return data.bound === true; });
-					var props = removeProps(true /* batch */);
-					var tpl = removeTemplate()(props(hasBound));
+					var max = Math.max(base, current);
+					max = Math.min(max, self.$("tr").length - 1);
 
-					oneshot.show(messages.get("label.Delete"), tpl).done(function() {
-						if (hasBound){
-							popupReindex();
-							}
-						var ids = rows.data().map(itemIdMapper).join(",");
-						$.ajax(self.apiRoot + "/" + ids, { type: "DELETE" })
-							.done(self.refresh);
-					});
+					return _.range(min, max + 1);
+				};
+
+				if (!c && !s) {
+					toggleSelection($tr);
+				} else if (c & !s) {
+					addToSelection($tr);
+				} else if (!c & s) {
+					growSelection($tr);
+				} else {
+					growSelection($tr);
 				}
-			});
+			},
 
-			return View;
+			onClickListDelete: function onClickListDelete(event) {
+				var self = this;
+				var tgt =  event.currentTarget;
+				var $tr = $(tgt).closest("tr");
+				var isBound = this.$el.DataTable().row($tr).data().bound === "true";
+				var props = removeProps(false /* not batch */);
+				var tpl = removeTemplate()(props(isBound));
+				oneshot.show(messages.get("label.Delete"), tpl, { width: "50%" }).done(function() {
+					isBound && popupReindex();
+					$.ajax(self.apiRoot + "/" + $(tgt).data("value"), { type: "DELETE" })
+						.done(self.refresh);
+				});
+			},
+
+			removeSelectedItems: function removeSelectedItems() {
+				var self = this;
+				var $sel = this.$(".ui-state-row-selected");
+				var rows = this.$el.DataTable().rows($sel);
+
+				if (rows.data().length === 0) {
+					notif.showWarning(messages.get("message.noListSelected"));
+					return;
+				}
+
+				var hasBound = _.some(rows.data(), function(data) { return data.bound === "true"; });
+				var props = removeProps(true /* batch */);
+				var tpl = removeTemplate()(props(hasBound));
+
+				oneshot.show(messages.get("label.Delete"), tpl, { width: "50%" }).done(function() {
+					hasBound && popupReindex();
+					var ids = rows.data().map(itemIdMapper).join(",");
+					$.ajax(self.apiRoot + "/" + ids, { type: "DELETE" })
+						.done(self.refresh);
+				});
+			}
 		});
+
+		return View;
+	});
