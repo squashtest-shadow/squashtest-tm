47e0d8c66d244a2ade19a8ab2fd065ff230d4dc5
gfouquet <gfouquet@henix.fr>
2015-04-23 11:43:25 +0200

    [Issue 4231][Issue 4232] In fact, ajax requests can now be async (take 3)

--- tm/tm.web/src/main/webapp/scripts/projects-manager/show-projects/NewProjectDialog.js
+++ tm/tm.web/src/main/webapp/scripts/projects-manager/show-projects/NewProjectDialog.js
@@ -18,8 +18,8 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define([ "jquery", "backbone", "handlebars", "app/lnf/Forms",
-		"jquery.squash.formdialog" ], function($, Backbone, Handlebars,  Forms) {
+define([ "jquery", "underscore", "backbone", "handlebars", "app/lnf/Forms",
+		"jquery.squash.formdialog" ], function($, _, Backbone, Handlebars,  Forms) {
 	"use strict";
 	/**
 	 * Saves the model
