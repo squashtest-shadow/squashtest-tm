169873c594892ecd9eefd1a7280d207c3216a94b
gfouquet <gfouquet@henix.fr>
2015-10-22 15:48:09 +0200

    [js] Fixed : after upgrade, 'jquery.form' would GET by default instead of POST

--- tm/tm.web/src/main/webapp/scripts/file-upload/jquery.squash.attachmentsDialog.js
+++ tm/tm.web/src/main/webapp/scripts/file-upload/jquery.squash.attachmentsDialog.js
@@ -186,6 +186,7 @@ define(
 
 						self.options._form.ajaxSubmit({
 							url: url + "?upload-ticket=" + ticket,
+							type: "post",
 							dataType: "text/html",
 							beforeSend: function (xhr) {
 								self.options._xhr = xhr;
