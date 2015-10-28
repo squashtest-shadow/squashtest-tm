dd3db9c47a7d9ec558ee7bc720b4e429a5646784
bsiri <bsiri@localhost>
2015-10-28 17:42:00 +0100

    [Issue 5456] now handling size exceeded exceptions for the downgraded upload dialog

--- tm/tm.web/src/main/java/org/squashtest/tm/web/internal/exceptionresolver/HandlerMaxUploadSizeExceptionResolver.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/internal/exceptionresolver/HandlerMaxUploadSizeExceptionResolver.java
@@ -22,19 +22,28 @@ package org.squashtest.tm.web.internal.exceptionresolver;
 
 import java.util.Map;
 
+import javax.inject.Inject;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 import org.springframework.web.servlet.view.AbstractView;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+import org.squashtest.tm.web.internal.i18n.InternationalizationHelper;
 
 @Component
 public class HandlerMaxUploadSizeExceptionResolver extends AbstractHandlerExceptionResolver {
 
+	private static final int NB_BYTES_PER_MBYTES = 1048576;
+	
+	@Inject
+	private InternationalizationHelper messageSource;
+	
 	public HandlerMaxUploadSizeExceptionResolver() {
 		super();
 	}
@@ -53,6 +62,8 @@ public class HandlerMaxUploadSizeExceptionResolver extends AbstractHandlerExcept
 				return handleAsJson(mex);
 			} else if (ExceptionResolverUtils.clientAcceptsMIME(request, MimeType.TEXT_PLAIN)) {
 				return handleAsText(mex);
+			} else if (ExceptionResolverUtils.clientAcceptsMIME(request, MimeType.TEXT_HTML)) {
+				return handleAsHtml(mex);
 			}
 			// special delivery for IE
 			else if (ExceptionResolverUtils.clientAcceptsMIME(request, MimeType.ANYTHING)) {
@@ -77,12 +88,28 @@ public class HandlerMaxUploadSizeExceptionResolver extends AbstractHandlerExcept
 		return new ModelAndView(view, "actionValidationError", error);
 
 	}
+	
+	private ModelAndView handleAsHtml(MaxUploadSizeExceededException mex) {
+
+		Long size = (mex.getMaxUploadSize() / NB_BYTES_PER_MBYTES);
+		
+		String msg = messageSource.getMessage("message.AttachmentUploadSizeExceeded",null, "message.AttachmentUploadSizeExceeded", LocaleContextHolder.getLocale()).replaceAll("#size#", size.toString());
+		
+		String error = "<div>"+msg+"</div>";
+
+		AbstractView view = new MaxSizeView();
+
+		return new ModelAndView(view, "actionValidationError", error);
+
+	}
 
 	private static class MaxSizeView extends AbstractView {
 		@Override
 		protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
 				HttpServletResponse response) throws Exception {
-			response.getOutputStream().write(((String) model.get("actionValidationError")).getBytes());
+			ServletOutputStream out = response.getOutputStream();
+			out.write(((String) model.get("actionValidationError")).getBytes());
+			out.flush();
 		}
 	}
 
