89f18e931ae9f5919036afbc289fb49ee6f2139d
juliensimon <juliensimon@localhost>
2016-01-25 12:32:39 +0100

    BSR fix some IT

--- integration-tests/src/it/groovy/org/squashtest/tm/service/internal/chart/ColumnPrototypeModificationIT.groovy
+++ integration-tests/src/it/groovy/org/squashtest/tm/service/internal/chart/ColumnPrototypeModificationIT.groovy
@@ -20,38 +20,37 @@
  */
 package org.squashtest.tm.service.internal.chart
 
+import javax.inject.Inject
+
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.transaction.annotation.Transactional
-import org.squashtest.tm.service.DbunitServiceSpecification
-
-import javax.inject.Inject;
-import org.springframework.context.ApplicationEventPublisher;
-import org.squashtest.tm.domain.customfield.CustomFieldBinding
-import org.squashtest.tm.event.CreateCustomFieldBindingEvent;
-import org.squashtest.tm.event.DeleteCustomFieldBindingEvent;
 import org.squashtest.tm.domain.customfield.BindableEntity
-import org.squashtest.tm.domain.chart.ColumnPrototype
-import org.squashtest.tm.event.ChangeCustomFieldCodeEvent
-
-import spock.unitils.UnitilsSupport
-
 import org.squashtest.tm.domain.customfield.CustomField
+import org.squashtest.tm.domain.customfield.CustomFieldBinding
+import org.squashtest.tm.event.ChangeCustomFieldCodeEvent
+import org.squashtest.tm.event.CreateCustomFieldBindingEvent
+import org.squashtest.tm.event.DeleteCustomFieldBindingEvent
+import org.squashtest.tm.service.DbunitServiceSpecification
 import org.unitils.dbunit.annotation.DataSet
 
+import spock.lang.Ignore
+import spock.unitils.UnitilsSupport
 @UnitilsSupport
 @Transactional
+@Ignore(value="The column prototype modification was implemented in 1.13 then desactivated because CUF in chart won't be part of 1.13. If the cuf part of 1.14 remove this annotation")
 @DataSet("ColumnPrototypeModification.dataset.xml")
 class ColumnPrototypeModificationIT extends DbunitServiceSpecification{
 
 	@Inject
 	ApplicationEventPublisher eventPublisher;
-	
+
 	def "should not create columnPrototype that already exist when new cufbinding is added" (){
-		
+
 		given:
-		
+
 		CustomField cuf = new CustomField(code:code)
 		CustomFieldBinding cufBinding = new CustomFieldBinding(customField:cuf, boundEntity :  entityType )
-		
+
 		when :
 		eventPublisher.publishEvent(new CreateCustomFieldBindingEvent(cufBinding));
 
@@ -63,92 +62,88 @@ class ColumnPrototypeModificationIT extends DbunitServiceSpecification{
 		"xx"        |        BindableEntity.TEST_CASE       || _
 		"xx"        |        BindableEntity.CAMPAIGN        || _
 	}
-	
+
 	def "should  create columnPrototype  when new cufbinding is added" (){
-		
+
 		given:
-		
+
 		CustomField cuf = new CustomField(code:code)
 		CustomFieldBinding cufBinding = new CustomFieldBinding(customField:cuf, boundEntity :  entityType )
-		
+
 		when :
 		eventPublisher.publishEvent(new CreateCustomFieldBindingEvent(cufBinding));
 
 		def result = findAll("ColumnPrototype");
 		then :
 		result.size == 7
-		
+
 		where :
 		code        |        entityType                     || _
 		"xx"        |        BindableEntity.ITERATION       || _
 		"ww"        |        BindableEntity.TEST_CASE       || _
 		"ww"        |        BindableEntity.CAMPAIGN        || _
-	
 	}
-	
-	
-	
-		def "should delete columnPrototype when cufbinding is removed and no other binding are using this columnPrototype" (){
-			
-			when :
-			eventPublisher.publishEvent(new DeleteCustomFieldBindingEvent(ids));
-	
-			then :
-			def result = findAll("ColumnPrototype");
-			result.size == remaining
-			where :
-			ids                                      || remaining
-			[-1L]                                    ||     6
-			[-2L]                                    ||     6
-			[-3L]                                    ||     5
-			[-4L]                                    ||     6
-			[-5L]                                    ||     6
-			[-6L]                                    ||     6
-			[-7L]                                    ||     6
-			[-8L]                                    ||     5
-			[-1L, -2L]                               ||     5
-			[-1L, -2L, -3L]                          ||     4
-			[-1L, -4L, -6L]                          ||     6
-			[-1L, -4L, -7L]                          ||     6
-			[-1L, -5L, -6L]                          ||     6
-			[-1L, -5L, -7L]                          ||     6
-			[-3L, -8L]                               ||     4
-			[-1L, -2L, -3L, -4L, -5L]                ||     3
-			[-1L, -2L, -3L, -4L, -5L, -6L, -8L]      ||     2
-			[-1L, -2L, -3L, -4L, -5L, -6L, -7L]      ||     2
-			[-1L, -2L, -3L, -4L, -5L, -6L, -7L, -8L] ||     1
-		}
-		
-		
-		def "should update column prototype when cuf code is changed" () {
-			when :
-			eventPublisher.publishEvent(new ChangeCustomFieldCodeEvent((String[]) codes));
-			def result = findAll("ColumnPrototype").findAll {return it.attributeName == codes[1]};
-			then :
-			result.size == number
-			where :
-			codes             || number
-			["xx", "AAAA"]    ||   2
-			["aa", "AAAA"]    ||   3
-			["ww", "AAAA"]    ||   0
-		}
-	
-		def "should delete filter when corresponding columnPrototype is deleted" (){
-			
-			when :
-			eventPublisher.publishEvent(new DeleteCustomFieldBindingEvent(ids));
-	
-			then :
-			def filters = findAll("Filter")
-			filters.size == remaining
-			where :
-			ids                                      || remaining
-			[-1L]                                    ||     1
-			[-2L]                                    ||     1
-			[-1L, -2L]                               ||     0
-			[-1L, -2L, -3L]                          ||     0
 
-		}
-	
-		
+
+
+	def "should delete columnPrototype when cufbinding is removed and no other binding are using this columnPrototype" (){
+
+		when :
+		eventPublisher.publishEvent(new DeleteCustomFieldBindingEvent(ids));
+
+		then :
+		def result = findAll("ColumnPrototype");
+		result.size == remaining
+		where :
+		ids                                      || remaining
+		[-1L]||     6
+		[-2L]||     6
+		[-3L]||     5
+		[-4L]||     6
+		[-5L]||     6
+		[-6L]||     6
+		[-7L]||     6
+		[-8L]||     5
+		[-1L, -2L]||     5
+		[-1L, -2L, -3L]||     4
+		[-1L, -4L, -6L]||     6
+		[-1L, -4L, -7L]||     6
+		[-1L, -5L, -6L]||     6
+		[-1L, -5L, -7L]||     6
+		[-3L, -8L]||     4
+		[-1L, -2L, -3L, -4L, -5L]||     3
+		[-1L, -2L, -3L, -4L, -5L, -6L, -8L]||     2
+		[-1L, -2L, -3L, -4L, -5L, -6L, -7L]||     2
+		[-1L, -2L, -3L, -4L, -5L, -6L, -7L, -8L]||     1
+	}
+
+
+	def "should update column prototype when cuf code is changed" () {
+		when :
+		eventPublisher.publishEvent(new ChangeCustomFieldCodeEvent((String[]) codes));
+		def result = findAll("ColumnPrototype").findAll {return it.attributeName == codes[1]};
+		then :
+		result.size == number
+		where :
+		codes             || number
+		["xx", "AAAA"]||   2
+		["aa", "AAAA"]||   3
+		["ww", "AAAA"]||   0
+	}
+
+	def "should delete filter when corresponding columnPrototype is deleted" (){
+
+		when :
+		eventPublisher.publishEvent(new DeleteCustomFieldBindingEvent(ids));
+
+		then :
+		def filters = findAll("Filter")
+		filters.size == remaining
+		where :
+		ids                                      || remaining
+		[-1L]||     1
+		[-2L]||     1
+		[-1L, -2L]||     0
+		[-1L, -2L, -3L]||     0
+	}
 }
