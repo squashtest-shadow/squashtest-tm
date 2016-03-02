91a3afcd8b8b65789fb0cd8445071d4d35822da5
gfouquet <gfouquet@henix.fr>
2016-03-02 15:47:33 +0100

    [spring sec] Restored the '/management' url namespace

--- tm/tm.web/src/main/java/org/squashtest/tm/web/config/WebSecurityConfig.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/config/WebSecurityConfig.java
@@ -20,14 +20,8 @@
  */
 package org.squashtest.tm.web.config;
 
-import static org.squashtest.tm.service.security.Authorizations.HAS_ROLE_ADMIN;
-import static org.squashtest.tm.service.security.Authorizations.HAS_ROLE_ADMIN_OR_PROJECT_MANAGER;
-
-import javax.inject.Inject;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.encoding.PasswordEncoder;
@@ -42,9 +36,14 @@ import org.springframework.web.filter.HttpPutFormContentFilter;
 import org.squashtest.tm.service.internal.security.SquashUserDetailsManager;
 import org.squashtest.tm.web.internal.filter.HtmlSanitizationFilter;
 
+import javax.inject.Inject;
+
+import static org.squashtest.tm.service.security.Authorizations.HAS_ROLE_ADMIN;
+import static org.squashtest.tm.service.security.Authorizations.HAS_ROLE_ADMIN_OR_PROJECT_MANAGER;
+
 /**
  * This configures Spring Security
- *
+ * <p/>
  * #configure(AuthenticationManagerBuilder) should not be overriden ! When it is overriden, it supersedes any "global"
  * AuthenticationManager. This means any third party authentication provider will be ignored.
  *
@@ -52,117 +51,7 @@ import org.squashtest.tm.web.internal.filter.HtmlSanitizationFilter;
  * @since 1.13.0
  */
 @Configuration
-@EnableConfigurationProperties(SquashManagementProperties.class)
 public class WebSecurityConfig {
-
-	@Configuration
-	@Order(1)
-	public static class SquashTAWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-				.csrf().disable()
-				.antMatcher("/automated-executions/**")
-				.authorizeRequests()
-					.anyRequest().access("hasRole('ROLE_TA_API_CLIENT')")
-					.and()
-				.httpBasic();
-		}
-	}
-
-	@Configuration
-	@Order(2)
-	public static class StandardWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-		@Value("${squash.security.filter.debug.enabled:false}")
-		private boolean debugSecurityFilter;
-		/*
-		 * @Inject private SquashManagementProperties managementProperties;
-		 *
-		 * @Inject private ServerProperties serverProperties;
-		 */
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.debug(debugSecurityFilter);
-		}
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http// When CSRF is on, a CSRF token is to be included in any POST/PUT/DELETE/PATCH request. This would require
-			// massive changes, so it's deactivated for now.
-			.csrf().disable()
-
-			.headers()
-			.cacheControl()
-			.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
-
-			.and().authorizeRequests()
-			.antMatchers(
-					"/administration",
-					"/administration/milestones",
-					"/administration/milestones/**",
-					"/administration/info-lists",
-					"/administration/info-lists/**",
-					"/administration/projects",
-					"/administration/projects/**",
-					"/milestone/**"
-					).access(HAS_ROLE_ADMIN_OR_PROJECT_MANAGER)
-			.antMatchers(
-					"/admin",
-					"/admin/**",
-					"/administration/**",
-					"/configuration",
-					"/configuration/**",
-					"/platform/**"
-					).access(HAS_ROLE_ADMIN)
-			.antMatchers("/accessDenied").permitAll()
-			.antMatchers("/management/**").denyAll()
-			.antMatchers("/resultUpdate/**").access("hasRole('ROLE_TA_API_CLIENT')")
-			.anyRequest().authenticated()
-
-			.and().formLogin()
-			.permitAll()
-			.loginPage("/login")
-			.failureUrl("/login?error")
-			.defaultSuccessUrl("/home-workspace")
-
-			.and().logout()
-			.permitAll()
-			.invalidateHttpSession(true)
-			.logoutSuccessUrl("/")
-
-			.and()
-			.addFilterAfter(new HttpPutFormContentFilter(), SecurityContextPersistenceFilter.class)
-			.addFilterAfter(new HtmlSanitizationFilter(), SecurityContextPersistenceFilter.class);
-			// @formatter:on
-
-			/*
-			 * RequestMatcher managementRequestMatcher = new RequestMatcher() {
-			 *
-			 * @Override public boolean matches(HttpServletRequest request) { return request.getLocalPort() ==
-			 * managementProperties.getPort(); } };
-			 */
-
-			// @formatter:off
-			// Secured namespace for the management api
-			// There is no authentication because this namespace collects system operation which cannot be done otherwise.
-			// As a consequence, is supposed to be secured at the app server / host level
-//		http.antMatcher("/management/**")
-//			// all http ports remapped to management port
-//			.portMapper()
-//				.http(80).mapsTo(managementProperties.getPort())
-//				.http(8080).mapsTo(managementProperties.getPort())
-//				.http(serverProperties.getPort() == null ? 8080 : serverProperties.getPort()).mapsTo(managementProperties.getPort())
-//			// all requests are secured
-//			.and().requiresChannel()
-//				.requestMatchers(managementRequestMatcher)
-//				.requires(managementProperties.getRequiredChannel())
-//			.and().authorizeRequests().anyRequest().permitAll();
-			// @formatter:on
-		}
-	}
-
 	/**
 	 * Defines a global internal (dao based) authentication manager. This is the default authentication manager.
 	 */
@@ -170,17 +59,111 @@ public class WebSecurityConfig {
 	@ConditionalOnProperty(name = "authentication.provider", matchIfMissing = true, havingValue = "internal")
 	@Order(0) // WebSecurityConfigurerAdapter default order is 100, we need to init this before
 	public static class InternalAuthenticationConfig extends GlobalAuthenticationConfigurerAdapter {
+		@Inject
+		private SquashUserDetailsManager squashUserDetailsManager;
+
+		@Inject
+		private PasswordEncoder passwordEncoder;
 
 		@Override
 		public void init(AuthenticationManagerBuilder auth) throws Exception {
 			auth.userDetailsService(squashUserDetailsManager).passwordEncoder(passwordEncoder);
 			auth.eraseCredentials(false);
 		}
+	}
 
-		@Inject
-		private SquashUserDetailsManager squashUserDetailsManager;
+	@Configuration
+	@Order(10)
+	public static class SquashTAWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.csrf().disable()
 
-		@Inject
-		private PasswordEncoder passwordEncoder;
+				.antMatcher("/automated-executions/**")
+					.authorizeRequests()
+						.anyRequest().access("hasRole('ROLE_TA_API_CLIENT')")
+				.and()
+				.httpBasic();
+			// @formatter:on
+		}
+	}
+
+	@Configuration
+	@Order(20)
+	public static class StandardWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Value("${squash.security.filter.debug.enabled:false}")
+		private boolean debugSecurityFilter;
+
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.debug(debugSecurityFilter)
+				.ignoring().antMatchers("/scripts/**");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				// When CSRF is on, a CSRF token is to be included in any POST/PUT/DELETE/PATCH request. This would require massive changes, so it's deactivated for now.
+				.csrf().disable()
+
+				.headers()
+				// w/o cache control, some browser's cache policy is too aggressive
+				.cacheControl()
+				.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+
+				.and()
+				.authorizeRequests()
+					// Administration namespace. Some of which can be accessed by PMs
+					.antMatchers(
+						"/administration",
+						"/administration/milestones",
+						"/administration/milestones/**",
+						"/administration/info-lists",
+						"/administration/info-lists/**",
+						"/administration/projects",
+						"/administration/projects/**",
+						"/milestone/**"
+					).access(HAS_ROLE_ADMIN_OR_PROJECT_MANAGER)
+					.antMatchers(
+						"/admin",
+						"/admin/**",
+						"/administration/**",
+						"/configuration",
+						"/configuration/**",
+						"/platform/**"
+					).access(HAS_ROLE_ADMIN)
+
+					.antMatchers("/accessDenied").permitAll()
+
+					// Squash TA callback api
+					.antMatchers("/resultUpdate/**").access("hasRole('ROLE_TA_API_CLIENT')")
+
+					// Namespace reserved for other use
+					.antMatchers("/management/**").denyAll()
+
+					.anyRequest().authenticated()
+
+				.and()
+				.formLogin()
+					.permitAll()
+					.loginPage("/login")
+					.failureUrl("/login?error")
+					.defaultSuccessUrl("/home-workspace")
+
+				.and()
+				.logout()
+					.permitAll()
+					.invalidateHttpSession(true)
+					.logoutSuccessUrl("/")
+
+				.and()
+				.addFilterAfter(new HttpPutFormContentFilter(), SecurityContextPersistenceFilter.class)
+				.addFilterAfter(new HtmlSanitizationFilter(), SecurityContextPersistenceFilter.class);
+			//@formatter:on
+		}
 	}
 }
+
