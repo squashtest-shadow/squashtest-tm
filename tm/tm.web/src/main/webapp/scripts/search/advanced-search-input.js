52d3ca200fb87e3b6710f8cd0c1103fb245ca0c9
jthebault <jthebault@localhost>
2016-04-06 15:29:39 +0200

    correcting empty perimeters in campaign search screen

--- tm/tm.web/src/main/webapp/scripts/search/advanced-search-input.js
+++ tm/tm.web/src/main/webapp/scripts/search/advanced-search-input.js
@@ -507,8 +507,14 @@ define([ "jquery", "backbone", "app/squash.handlebars.helpers", "squash.translat
 				var attrName = key == 'project.id' ? 'project' : 'resid';
 
 				var ids =_.map(selectedInTree, function(node){return $(node).attr(attrName);});
+                if (ids!=undefined&&ids.length>0) {
+                    jsonVariable[key] = {type : "LIST", values : ids};
+                }
+                else {
+                    jsonVariable["project.id"] = {type : "LIST", values : []};
+                }
 
-				jsonVariable[key] = {type : "LIST", values : ids};
+				
 			}
 
 
