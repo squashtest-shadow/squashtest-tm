801bc3707fe3e0ac45ff365b419c3a6877e3d8a5
jthebault <jthebault@localhost>
2016-04-07 14:51:23 +0200

    JPA CustomReportLibraryDao

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/CustomReportLibraryDao.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/repository/CustomReportLibraryDao.java
@@ -20,10 +20,12 @@
  */
 package org.squashtest.tm.service.internal.repository;
 
-import org.squashtest.tm.core.dynamicmanager.annotation.DynamicDao;
+import org.springframework.data.repository.Repository;
 import org.squashtest.tm.domain.customreport.CustomReportLibrary;
 
-@DynamicDao(entity = CustomReportLibrary.class)
-public interface CustomReportLibraryDao extends CustomCustomReportLibraryDao {
-	
+import java.util.List;
+
+public interface CustomReportLibraryDao extends Repository<CustomReportLibrary, Long> {
+
+	List<CustomReportLibrary> findAll();
 }
