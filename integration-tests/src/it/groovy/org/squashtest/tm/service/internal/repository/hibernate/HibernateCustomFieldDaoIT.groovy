e469e26cfcb018f49d5c73a454af95acc3c5d128
gfouquet <gfouquet@henix.fr>
2016-04-13 14:36:41 +0200

    [spring-data] Fixed ITs

--- integration-tests/src/it/groovy/org/squashtest/tm/service/internal/repository/hibernate/HibernateCustomFieldDaoIT.groovy
+++ integration-tests/src/it/groovy/org/squashtest/tm/service/internal/repository/hibernate/HibernateCustomFieldDaoIT.groovy
@@ -23,9 +23,11 @@ package org.squashtest.tm.service.internal.repository.hibernate
 import org.squashtest.tm.core.foundation.collection.PagingAndSorting
 import org.squashtest.tm.core.foundation.collection.SortOrder
 import org.squashtest.tm.domain.customfield.CustomField
+import org.squashtest.tm.domain.customfield.CustomFieldBinding
 import org.squashtest.tm.domain.customfield.InputType
 import org.squashtest.tm.service.internal.repository.CustomFieldDao
 import org.unitils.dbunit.annotation.DataSet
+import spock.lang.Ignore
 import spock.unitils.UnitilsSupport
 
 import javax.inject.Inject
@@ -38,7 +40,7 @@ class HibernateCustomFieldDaoIT extends DbunitDaoSpecification {
 	@DataSet("HibernateCustomFieldDaoIT.should return list of cuf ordered by name.xml")
 	def "should return list of cuf ordered by name" () {
 		when:
-		List<CustomField> list = customFieldDao.findAllOrderedByName()
+		List<CustomField> list = customFieldDao.findAllByOrderByNameAsc()
 
 		then:
 		list.size() == 3
@@ -46,19 +48,19 @@ class HibernateCustomFieldDaoIT extends DbunitDaoSpecification {
 		list.get(1).name == "cde"
 		list.get(2).name == "fde"
 	}
-	
+
 	@DataSet("HibernateCustomFieldDaoIT.should return sorted list of cuf.xml")
 	def "should return sorted list of cuf"(){
 		when:
 		List<CustomField> list = customFieldDao.findSortedCustomFields(new InputTypeCollectionFilter())
-		
-		then: 
+
+		then:
 		list.size() == 2
 		list.get(0).inputType == InputType.DROPDOWN_LIST
 		list.get(1).inputType == InputType.PLAIN_TEXT
 
 	}
-	
+
 	private class InputTypeCollectionFilter implements PagingAndSorting	{
 			@Override
 			String getSortedAttribute(){
@@ -72,64 +74,61 @@ class HibernateCustomFieldDaoIT extends DbunitDaoSpecification {
 			public int getPageSize() {
 				return 2;
 			}
-			/* (non-Javadoc)
-			 * @see org.squashtest.tm.core.foundation.collection.Sorting#getSortOrder()
-			 */
 			@Override
 			public SortOrder getSortOrder() {
 				return SortOrder.ASCENDING;
 			}
-			
+
 			@Override
 			public boolean shouldDisplayAll() {
 				return false;
 			}
 	}
-	
-	//TODO un-comment when deletion is handled
-//	@DataSet("HibernateCustomFieldDeletionDaoIT.should delete custom field.xml")
-//	def "should delete custom field" () {
-//		given:
-//		CustomField cuf =  findEntity(CustomField.class, -1L)
-//		when:
-//		customFieldDao.remove(cuf)
-//		then:
-//		!found(CustomField.class, -1L)
-//		found(CustomField.class, -2L)
-//	}
-//	
-//	@DataSet("HibernateCustomFieldDeletionDaoIT.should delete custom field and option.xml")
-//	def "should delete custom field and options" () {
-//		given:
-//		CustomField cuf =  findEntity(CustomField.class, -1L)
-//		when:
-//		customFieldDao.remove(cuf)
-//		then:
-//		!found(CustomField.class, -1L)
-//		!foundCustomFieldOption("first")
-//		!foundCustomFieldOption("second")
-//		!foundCustomFieldOption("third")
-//	}
-//	
-//	
-//	@DataSet("HibernateCustomFieldDeletionDaoIT.should delete custom field and binding.xml")
-//	def "should delete custom field and binding " () {
-//		given:
-//		CustomField cuf =  findEntity(CustomField.class, -1L)
-//		when:
-//		customFieldDao.remove(cuf)
-//		then:
-//		!found(CustomField.class, -1L)
-//		!found(CustomFieldBinding.class, -1L)
-//		!found(CustomFieldBinding.class, -2L)
-//	}
-//	
-//	
-///* --------------------	private utilities --------------------------*/
-//	private foundCustomFieldOption(String label){
-//		Query query = session.createQuery("from CustomFieldOption cufo where cufo.label = :label")
-//		query.setParameter("label", label);
-//		return query.uniqueResult() != null;
-//	}
-	
+
+	@Ignore("un-comment when deletion is handled")
+	@DataSet("HibernateCustomFieldDeletionDaoIT.should delete custom field.xml")
+	def "should delete custom field" () {
+		given:
+		CustomField cuf =  findEntity(CustomField.class, -1L)
+		when:
+		customFieldDao.delete(cuf)
+		then:
+		!found(CustomField.class, -1L)
+		found(CustomField.class, -2L)
+	}
+
+	@Ignore("un-comment when deletion is handled")
+	@DataSet("HibernateCustomFieldDeletionDaoIT.should delete custom field and option.xml")
+	def "should delete custom field and options" () {
+		given:
+		CustomField cuf =  findEntity(CustomField.class, -1L)
+		when:
+		customFieldDao.delete(cuf)
+		then:
+		!found(CustomField.class, -1L)
+		!foundCustomFieldOption("first")
+		!foundCustomFieldOption("second")
+		!foundCustomFieldOption("third")
+	}
+
+
+	@Ignore("un-comment when deletion is handled")
+	@DataSet("HibernateCustomFieldDeletionDaoIT.should delete custom field and binding.xml")
+	def "should delete custom field and binding " () {
+		given:
+		CustomField cuf =  findEntity(CustomField.class, -1L)
+		when:
+		customFieldDao.delete(cuf)
+		then:
+		!found(CustomField.class, -1L)
+		!found(CustomFieldBinding.class, -1L)
+		!found(CustomFieldBinding.class, -2L)
+	}
+
+	private foundCustomFieldOption(String label){
+		entityManager.createQuery("select count(cufo) from CustomFieldOption cufo where cufo.label = :label")
+			.setParameter("label", label)
+			.singleResult == 1
+	}
+
 }
