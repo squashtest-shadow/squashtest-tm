d2b5a4395968ddcc096731ab298468800c64bdc9
gfouquet <gfouquet@henix.fr>
2016-06-08 17:35:35 +0200

    license

--- tm/tm.web/src/main/webapp/scripts/workspace/Storage.js
+++ tm/tm.web/src/main/webapp/scripts/workspace/Storage.js
@@ -17,8 +17,6 @@
  *
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
  */
 /*
  * This is a simple web storage wrapper that handles object values, if we need something more
