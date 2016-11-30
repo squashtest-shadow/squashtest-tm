6390a8a79d18edfd5f0b5b1872bf105bb4abf40c
Grelotine <Grelotine@localhost>
2016-11-30 17:26:08 +0100

    Changed logger level in IndexationServiceImpl

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/advancedsearch/IndexationServiceImpl.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/advancedsearch/IndexationServiceImpl.java
@@ -278,7 +278,7 @@ public class IndexationServiceImpl implements IndexationService {
 			}
 
 		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			LOGGER.error("Error during indexing", e);
+			LOGGER.debug("Error during indexing", e);
 		}
 
 	}
