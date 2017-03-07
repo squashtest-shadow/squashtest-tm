58edc5449aa0bd4b8acb0d52fc3e8a3decef58b6
Grelotine <Grelotine@localhost>
2017-03-07 14:32:47 +0100

    [Feat #6735] Checkbox to copy test plan must be checked by default.

--- tm/tm.web/src/main/webapp/scripts/camp-workspace/popups/add-iteration-popup.js
+++ tm/tm.web/src/main/webapp/scripts/camp-workspace/popups/add-iteration-popup.js
@@ -18,9 +18,9 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
-define(['jquery', 'tree', 'custom-field-values', 'workspace.projects', '../permissions-rules', 'workspace.routing', 'jquery.squash.formdialog'], 
+define(['jquery', 'tree', 'custom-field-values', 'workspace.projects', '../permissions-rules', 'workspace.routing', 'jquery.squash.formdialog'],
 		function($, zetree, cufValuesManager, projects, rules, routing){
-	
+
 	function postNode(dialog, tree){
 
 		var params = {
@@ -29,56 +29,56 @@ define(['jquery', 'tree', 'custom-field-values', 'workspace.projects', '../permi
 			description : dialog.find('#add-iteration-description').val(),
 			copyTestPlan : dialog.find("#copy-test-plan-box").is(':checked')
 		};
-		
+
 		var cufParams = dialog.data('cuf-values-support').readValues();
-		
+
 		$.extend(params, cufParams);
-		
+
 		return tree.jstree('postNewNode', 'new-iteration', params, true);
 	}
-	
-	
+
+
 	function addCufHandler(dialog, tree){
 		var table = dialog.find('table.add-node-attributes');
 		var cufHandler = cufValuesManager.newCreationPopupCUFHandler({table : table});
-		
+
 		dialog.on('formdialogopen', function(){
 			var projectId = tree.jstree('get_selected').getProjectId();
 			var bindings = projects.findProject(projectId).customFieldBindings['ITERATION'];
 			var cufs = $.map(bindings, function(b){return b.customField;});
-			
-			cufHandler.loadPanel(cufs);	
+
+			cufHandler.loadPanel(cufs);
 		});
-		
+
 		dialog.on('formdialogcleanup', function(){
 			cufHandler.reset();
 		});
-		
+
 		dialog.on('formdialogclose', function(){
 			cufHandler.destroy();
 		});
-		
+
 		dialog.data('cuf-values-support', cufHandler);
 	}
-	
+
 	function fetchIterationCount(node){
 		var cId = node.getResId();
 		var url = routing.buildURL('campaigns.countIterations', cId);
 		return $.get(url, 'json');
 	}
-	
+
 	function init(){
-		
+
 		var dialog = $("#add-iteration-dialog").formDialog();
 		var tree = zetree.get();
-		
+
 		// Added to cancel the open if no rights
 		dialog.on('formdialogopen', function(){
 			var node = tree.jstree('get_selected');
-			
+
 			if (! rules.canCreateIteration(node)){
-				/* Acknowledged by Safi, David and Gregory : 
-				 * Inactivated buttons and item-menu should not be clickable. 
+				/* Acknowledged by Safi, David and Gregory :
+				 * Inactivated buttons and item-menu should not be clickable.
 				 * No error popup should be visible despite the fact a lot of these error popup have been implemented since 1.11.
 				 * Due to event flow bug in jQuery, we cant't prevent the popup 'onopen' event to be triggered,
 				 * so it was decided to close the popup immediately if rules check function return false,
@@ -99,47 +99,50 @@ define(['jquery', 'tree', 'custom-field-values', 'workspace.projects', '../permi
 				.success(function(cnt){
 					dialog.formDialog("option", 'itercount', cnt);
 					dialog.find('#add-iteration-reference').val((cnt+1));
-					
+
 					dialog.formDialog('setState','confirm');
 					var name = node.getName();
-					dialog.find("#new-iteration-tree-button").val(name);						
-				});			
-			}			
+					dialog.find("#new-iteration-tree-button").val(name);
+				});
+			}
 		});
 
 		// end
-		
+
 		dialog.on('formdialogcleanup', function(){
 			var cnt = dialog.formDialog('option', 'itercount') || '--';
 			dialog.find('#add-iteration-reference').val((cnt+1));
-			/* Issue #6420: The checkbox 'copy test plan' must 
-			 * not be checked by default. */
-			dialog.find('#copy-test-plan-box').attr('checked', false);
+			/* Issue #6735: The checkbox 'copy campaign test plan' must
+			 * be checked by default. */
+			 var copyCheckBox = dialog.find("#copy-test-plan-box");
+			 if(!copyCheckBox.is(':checked')) {
+			 	copyCheckBox.click();
+			 }
 		});
 
 		dialog.on('formdialogadd-close', function(){
 			postNode(dialog,tree).then(function(){
 				dialog.formDialog('close');
-			});			
+			});
 		});
-		
+
 		dialog.on('formdialogadd-another', function(){
 			postNode(dialog, tree).then(function(){
 				var cnt = dialog.formDialog('option', 'itercount');
 				dialog.formDialog('option', 'itercount', (cnt+1));
 				dialog.formDialog('cleanup');
-			}) ;		
+			}) ;
 		});
-		
+
 		dialog.on('formdialogcancel', function(){
 			dialog.formDialog('close');
 		});
-		
-		
+
+
 		addCufHandler(dialog, tree);
-		
+
 	}
-	
+
 	return {
 		init : init
 	};
