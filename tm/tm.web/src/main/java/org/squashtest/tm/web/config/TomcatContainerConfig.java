c0e3e60d55d7f53c44848526275fba6527d8ab79
bsiri <bsiri@localhost>
2017-03-21 16:15:33 +0100

    [Issue 6757] fixed, the Tomcat servlet container has now custom code that supplies the connector with a plain path to the keystore (as expected) instead of an URL. Note that this fix works fine as long as we do not update Tomcat version, which will be the case for the 1.15 branch. However for branch 1.16 and onward we should focus on using a more recent Tomcat version (Spring boot 1.4 expects Tomcat 8.5), and then deal with the other problems encountered for that version (performances etc)

--- tm/tm.web/src/main/java/org/squashtest/tm/web/config/TomcatContainerConfig.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/config/TomcatContainerConfig.java
@@ -25,15 +25,22 @@
  */
 package org.squashtest.tm.web.config;
 
+import java.io.FileNotFoundException;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.webresources.StandardRoot;
+import org.apache.coyote.http11.AbstractHttp11JsseProtocol;
 import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerException;
+import org.springframework.boot.context.embedded.Ssl;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 
 /**
  * Initiated as a workaround  for http://stackoverflow.com/questions/31605129/spring-file-upload-with-multipart-resolver-causes-connection-reset-when-file-is
@@ -42,6 +49,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class TomcatContainerConfig {
+	
 
     /**
      * This method defines a modified servlet container that will run Squash TM when deployed as a standalone application.
@@ -53,24 +61,120 @@ public class TomcatContainerConfig {
     @Bean
     @ConditionalOnProperty(prefix="squash", name="run-as-war", havingValue = "false", matchIfMissing = true)
     public TomcatEmbeddedServletContainerFactory containerFactory(){
-		TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory = new TomcatEmbeddedServletContainerFactory() {
-			@Override
-			protected void customizeConnector(Connector connector) {
-				super.customizeConnector(connector);
-				if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
-					((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
-				}
-			}
-
-			@Override
-			protected void postProcessContext(Context context) {
-				final int cacheSize = 40 * 1024;
-				StandardRoot standardRoot = new StandardRoot(context);
-				standardRoot.setCacheMaxSize(cacheSize);
-				context.setResources(standardRoot);
-			}
-		};
+		
+    	TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory = new CustomizedTomcatContainerFactory();
 
 		return tomcatEmbeddedServletContainerFactory;
 	}
+    
+    
+    
+    private static final class CustomizedTomcatContainerFactory extends TomcatEmbeddedServletContainerFactory {
+    	
+    	private static final Logger LOGGER = LoggerFactory.getLogger(CustomizedTomcatContainerFactory.class);
+    	
+    	
+    	@Override
+    	protected void customizeConnector(Connector connector) {
+    		super.customizeConnector(connector);
+    		if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+    			((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+    		}
+    	}
+
+    	@Override
+    	protected void postProcessContext(Context context) {
+    		final int cacheSize = 40 * 1024;
+    		StandardRoot standardRoot = new StandardRoot(context);
+    		standardRoot.setCacheMaxSize(cacheSize);
+    		context.setResources(standardRoot);
+    	}
+
+    	/*
+    	 * Issue 6757
+    	 * 
+    	 * As of Squash 1.15.1, the use of Spring Boot 1.4.0 with Tomcat 8.0.23 breaks the creation of an 
+    	 * SSL connector, because Spring expected to configure a Tomcat 8.5. Specifically it supplies the 
+    	 * connector with the URL of the keystore, while the connector expects a plain path to the file. 
+    	 * After some boilerplate, the final path to the keystore is malformed and cannot be found.
+    	 * 
+    	 * This fix attempts to configure the paths to keystore and trustore, in case they are 
+    	 * hosted on the filesystem. This will not work for classpath-hosted keystore, but we don't 
+    	 * use that feature in Squash.
+    	 * 
+    	 * This fix patches the problem for the version 1.15. The proper fix for 1.16 would rather 
+    	 * rely on the correct version of Tomcat, and fix what is wrong with it instead. 
+    	 */
+    	@Override
+    	protected void configureSsl(AbstractHttp11JsseProtocol<?> protocol, Ssl ssl) {
+    		// first, call the super method as usual
+    		super.configureSsl(protocol, ssl);
+    		
+    		// now, if SSL is not supplied by an internal provider, 
+    		// reconfigure it using the correct path 
+    		// (the following is adapted from the super method)
+    		if (getSslStoreProvider() == null) {
+    			configureSslKeyStore(protocol, ssl);
+    			configureSslTrustStore(protocol, ssl);
+    		}
+    		
+    	}
+
+    	// copy pasta of super.configureSsslKeyStore
+    	private void configureSslKeyStore(AbstractHttp11JsseProtocol<?> protocol, Ssl ssl) {
+    		try {
+    			String ksUrl = ResourceUtils.getURL(ssl.getKeyStore()).toString();
+    			String ksPath = removeProtocol(ksUrl);
+    			protocol.setKeystoreFile(ksPath);
+    			
+    			if (LOGGER.isInfoEnabled()){
+    				LOGGER.info("Issue 6757 : fixed path to keystore is '"+ksPath+"'");
+    			}
+    		}
+    		catch (FileNotFoundException ex) {
+    			throw new EmbeddedServletContainerException(
+    					"Could not load key store: " + ex.getMessage(), ex);
+    		}
+    		if (ssl.getKeyStoreType() != null) {
+    			protocol.setKeystoreType(ssl.getKeyStoreType());
+    		}
+    		if (ssl.getKeyStoreProvider() != null) {
+    			protocol.setKeystoreProvider(ssl.getKeyStoreProvider());
+    		}
+    	}
+
+    	// copy pasta of super.configureSsslTrustStore
+    	private void configureSslTrustStore(AbstractHttp11JsseProtocol<?> protocol, Ssl ssl) {
+
+    		if (ssl.getTrustStore() != null) {
+    			try {
+    				String tsUrl = ResourceUtils.getURL(ssl.getTrustStore()).toString();
+    				String tsPath = removeProtocol(tsUrl);
+    				protocol.setTruststoreFile(tsPath);
+    				
+        			if (LOGGER.isInfoEnabled()){
+        				LOGGER.info("Issue 6757 : fixed path to truststore is '"+tsPath+"'");
+        			}
+    			}
+    			catch (FileNotFoundException ex) {
+    				throw new EmbeddedServletContainerException(
+    						"Could not load trust store: " + ex.getMessage(), ex);
+    			}
+    		}
+    		protocol.setTruststorePass(ssl.getTrustStorePassword());
+    		if (ssl.getTrustStoreType() != null) {
+    			protocol.setTruststoreType(ssl.getTrustStoreType());
+    		}
+    		if (ssl.getTrustStoreProvider() != null) {
+    			protocol.setTruststoreProvider(ssl.getTrustStoreProvider());
+    		}
+    	}
+
+    	private String removeProtocol(String toclean){
+    		// we remove file: if specified, 
+    		// if protocol classpath: is used we just let it go and see whatever happens 
+    		return toclean.replaceAll("(file:)", "");
+    	}
+    }
+    
 }
