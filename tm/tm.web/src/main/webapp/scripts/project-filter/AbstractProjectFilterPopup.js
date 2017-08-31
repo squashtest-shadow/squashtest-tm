f83c2bf23293de825f2a98df370ebc4deaedc048
zhening <zhening@localhost>
2017-08-31 11:32:33 +0200

    [Issue 5125] Fixed project filter issue for certain characters

--- tm/tm.web/src/main/webapp/scripts/project-filter/AbstractProjectFilterPopup.js
+++ tm/tm.web/src/main/webapp/scripts/project-filter/AbstractProjectFilterPopup.js
@@ -21,14 +21,14 @@
 /**
  * projectPicker JQuery ui widget. Should be used with the appropriate dom
  * component (project-picker.frag.html)
- * 
+ *
  * Configuration : { url: "the url where to get the projects", required confirm :
  * function() on confirm, required cancel: function() on cancel, optional,
  * defaults to close loadOnce: true|false|"never", loads projects only once,
  * defaults to false, loads on each open, "never" never loads. }
- * 
+ *
  * It also forwards additional configuration to the internal popup dialog.
- * 
+ *
  * Methods : open, close
  */
  define([ "jquery",  "./ProjectFilterModel","underscore", "squashtable", "jqueryui", "jquery.squash.confirmdialog"],
@@ -39,10 +39,10 @@
 			$boxes.each(eachCallback);
 			return _.pluck($boxes, 'value');
 	}
-		
-	
+
+
 		var AbstractProjectFilterPopup = Backbone.View.extend({
-		 
+
 			events : {
 				"confirmdialogclose" : "close",
 				"confirmdialogcancel" : "cancel",
@@ -52,7 +52,7 @@
 				"click .project-picker-deselall" : "deselectAllProjects",
 				"click .project-picker-invsel" : "invertAllProjects"
 			},
-		 
+
 			_initialize : function() {
                 var self = this;
                 this.filterTable = $.proxy(this._filterTable, this);
@@ -67,28 +67,32 @@
                         ids.push(id);
                     }
                 });
-                // set model 
+                // set model
                 var url  = this.$el.data("url");
                 self.model = new ProjectFilterModel({projectIds : ids} );
                 self.model.url = url;
-                
+
                 // init confirm dialog
                 this.$el.confirmDialog({width : 800});
-                
+
                 // init datatable
                 // change filter by search.dt
                 self.table = this.$el.find("table").bind('search.dt', self.filterTable).squashTable({
                     "sScrollY": "500px",
                     "bFilter":true,
-                    "bPaginate" : false, 
+                    "bPaginate" : false,
                     "bServerSide" : false,
                     "bScrollCollapse": true,
                     "bAutoWidth" : true,
                     "bRetrieve" : false,
-                    "sDom" : '<"H"lfr>t'
+                    "sDom" : '<"H"lfr>t',
+										"aoColumnDefs": [{
+											"aTargets": [0],
+											"bSearchable": false
+										}]
                 });
             },
-			
+
 			open : function(){
 				this.$el.confirmDialog("open");
 				this.table.fnAdjustColumnSizing();
@@ -106,7 +110,7 @@
 			confirm : function(){
 				throw "You must define confirm in objects deriving from this view";
 			},
-			
+
 			cancel : function(){
 				this.table.fnFilter( '' );
 				this.$el.find(".project-checkbox").each(function() {
@@ -114,27 +118,27 @@
 					this.checked = previous;
 				});
 			},
-	 
+
 			dialogConfig : {
 				autoOpen : false,
 				resizable : false,
 				modal : true
 			},
-		
+
 			selectAllProjects : function() {
 				var ids = eachCheckbox(this.$el, function() {
 					this.checked = true;
 				});
 				this.model.select(ids);
 			},
-		
+
 			deselectAllProjects : function () {
 				var ids = eachCheckbox(this.$el, function() {
 					this.checked = false;
 				});
 				this.model.deselect(ids);
 			},
-		
+
 			invertAllProjects : function () {
 				var selectIds = [];
 				var deselectIds = [];
@@ -145,17 +149,17 @@
 					}else{
 						this.checked = true;
 						selectIds.push(this.value);
-						
+
 					}
-					
+
 				});
 				this.model.select(selectIds);
 				this.model.deselect(deselectIds);
 			},
-			
+
 			notifyModel : function(event){
 				var checkbox = event.currentTarget;
-				this.model.changeProjectState(checkbox.value, checkbox.checked);				
+				this.model.changeProjectState(checkbox.value, checkbox.checked);
 			},
 
 			// This popup is bugged as hell. It fire a close event on confirm and closing by click on the closethick
@@ -174,6 +178,6 @@
 		view.prototype.events = _.extend({}, this.prototype.events, child.events);
 		return view;
 	};
-	 
+
 		return AbstractProjectFilterPopup;
 });
