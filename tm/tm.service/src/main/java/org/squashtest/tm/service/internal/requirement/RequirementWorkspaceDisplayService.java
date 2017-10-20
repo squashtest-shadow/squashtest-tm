44bed543ec31fb173fc056d0ad233eb1de1e9d4b
ChrisHolc <ChrisHolc@localhost>
2017-10-20 16:21:38 +0200

    Requirement tree - icons

--- tm/tm.service/src/main/java/org/squashtest/tm/service/internal/requirement/RequirementWorkspaceDisplayService.java
+++ tm/tm.service/src/main/java/org/squashtest/tm/service/internal/requirement/RequirementWorkspaceDisplayService.java
@@ -31,6 +31,7 @@ import org.squashtest.tm.domain.requirement.RequirementLibrary;
 import org.squashtest.tm.domain.requirement.RequirementLibraryPluginBinding;
 import org.squashtest.tm.jooq.domain.tables.*;
 import org.squashtest.tm.service.internal.dto.json.JsTreeNode;
+import org.squashtest.tm.service.internal.dto.json.JsTreeNode.State;
 import org.squashtest.tm.service.internal.workspace.AbstractWorkspaceDisplayService;
 
 import javax.inject.Inject;
@@ -47,17 +48,14 @@ public class RequirementWorkspaceDisplayService extends AbstractWorkspaceDisplay
 	@Inject
 	DSLContext DSL;
 
-	private RequirementLibraryContent RLC = REQUIREMENT_LIBRARY_CONTENT.as("RLC");
 	private RequirementLibraryNode RLN = REQUIREMENT_LIBRARY_NODE.as("RLN");
-	private RequirementLibraryNode RLN_CHILD = REQUIREMENT_LIBRARY_NODE.as("RLN_CHILD");
 	private RequirementFolder RF = REQUIREMENT_FOLDER.as("RF");
 	private Requirement REQ = REQUIREMENT.as("REQ");
 	private RequirementVersion RV = REQUIREMENT_VERSION.as("RV");
-	private RequirementVersionCoverage RVC = REQUIREMENT_VERSION_COVERAGE.as("RVC");
 	private RlnRelationship RLNR = RLN_RELATIONSHIP.as("RLNR");
 	private Resource RES = RESOURCE.as("RES");
-	MilestoneReqVersion MRV = MILESTONE_REQ_VERSION.as("MRV");
-
+	private MilestoneReqVersion MRV = MILESTONE_REQ_VERSION.as("MRV");
+	private InfoListItem ILI = INFO_LIST_ITEM.as("ILI");
 
 	@Override
 	protected Map<Long, JsTreeNode> getChildren(MultiMap fatherChildrenLibrary, MultiMap fatherChildrenEntity) {
@@ -65,6 +63,7 @@ public class RequirementWorkspaceDisplayService extends AbstractWorkspaceDisplay
 		childrenIds.addAll(fatherChildrenLibrary.values());
 		childrenIds.addAll(fatherChildrenEntity.keySet());
 		childrenIds.addAll(fatherChildrenEntity.values());
+
 		return DSL
 			.select(
 				RLN.RLN_ID,
@@ -75,14 +74,13 @@ public class RequirementWorkspaceDisplayService extends AbstractWorkspaceDisplay
 				REQ.MODE,
 				RV.REFERENCE,
 				MRV.MILESTONE_ID,
-//				org.jooq.impl.DSL.decode()
-//					.when(RVC.VERIFYING_TEST_CASE_ID.isNull(), "false")
-//					.otherwise("true")
-//					.as("IS_REQ_COVERED"),
 				org.jooq.impl.DSL.decode()
 					.when(RLNR.ANCESTOR_ID.isNull(), "false")
 					.otherwise("true")
-					.as("HAS_CONTENT")
+					.as("HAS_CONTENT"),
+				org.jooq.impl.DSL.decode()
+					.when(ILI.ICON_NAME.eq(""), "def_cat_noicon")
+					.otherwise(ILI.ICON_NAME).as("ICON_NAME")
 			)
 			.from(RLN)
 			.leftJoin(RF).on(RLN.RLN_ID.eq(RF.RLN_ID))
@@ -91,6 +89,7 @@ public class RequirementWorkspaceDisplayService extends AbstractWorkspaceDisplay
 			.leftJoin(RV).on(RLN.RLN_ID.eq(RV.REQUIREMENT_ID))
 			.leftJoin(MRV).on(REQ.CURRENT_VERSION_ID.eq(MRV.REQ_VERSION_ID))
 			.leftJoin(RLNR).on(RLN.RLN_ID.eq(RLNR.ANCESTOR_ID))
+			.leftJoin(ILI).on(RV.CATEGORY.eq(ILI.ITEM_ID.cast(Long.class)))
 			.where(RLN.RLN_ID.in(childrenIds))
 			.groupBy(RLN.RLN_ID)
 			.fetch()
@@ -100,26 +99,46 @@ public class RequirementWorkspaceDisplayService extends AbstractWorkspaceDisplay
 					return buildFolder(r.get(RLN.RLN_ID), r.get(RES.NAME), (String) r.get("RESTYPE"), (String) r.get("HAS_CONTENT"));
 				} else {
 					return buildRequirement(r.get(RLN.RLN_ID), r.get(RES.NAME), (String) r.get("RESTYPE"), r.get(RV.REFERENCE),
-						r.get(REQ.MODE), r.get(MRV.MILESTONE_ID));
+						r.get(REQ.MODE), r.get(MRV.MILESTONE_ID), (String) r.get("ICON_NAME"), (String) r.get("HAS_CONTENT"));
 				}
 			})
 			.collect(Collectors.toMap(node -> (Long) node.getAttr().get("resId"), Function.identity()));
 	}
 
-	private JsTreeNode buildRequirement(Long id, String name, String restype, String reference, String mode, Long milestone) {
+	private JsTreeNode buildRequirement(Long id, String name, String restype, String reference, String mode, Long milestone, String categoryIcon, String hasContent) {
 		Map<String, Object> attr = new HashMap<>();
-
+		State state;
 		attr.put("resId", id);
 		attr.put("resType", restype);
 		attr.put("name", name);
-		attr.put("id", "TestCase-" + id);
-		attr.put("rel", "test-case");
-
+		attr.put("id", "Requirement-" + id);
+		attr.put("rel", "requirement");
 		attr.put("reference", reference);
-		attr.put("mode", mode);
+		if (mode.equals("SYNCHRONIZED"))
+			attr.put("synchronized", true);
 		attr.put("milestone", milestone);
+		attr.put("category-icon", categoryIcon);
 
-		return buildNode(name, JsTreeNode.State.leaf, attr);
+		if (Boolean.parseBoolean(hasContent)) {
+			state = JsTreeNode.State.closed;
+		} else {
+			state = State.leaf;
+		}
+
+		return buildNode(name, state, attr);
+	}
+
+	public List<Long> findReqsWithChildrenLinkedToMilestone(List<Long> reqVersionIdsWithMilestone) {
+		List<Long> reqIdsWithMilestone = DSL.select().from(REQUIREMENT_VERSION)
+			.where(REQUIREMENT_VERSION.RES_ID.in(reqVersionIdsWithMilestone))
+			.fetch(REQUIREMENT_VERSION.REQUIREMENT_ID, Long.class);
+
+		return DSL.selectDistinct()
+			.from(RLN_RELATIONSHIP_CLOSURE)
+			.where(RLN_RELATIONSHIP_CLOSURE.DESCENDANT_ID.in(reqIdsWithMilestone)
+				.and(RLN_RELATIONSHIP_CLOSURE.ANCESTOR_ID.notIn(DSL.select(REQUIREMENT_FOLDER.RLN_ID).from(REQUIREMENT_FOLDER)))
+				.and(RLN_RELATIONSHIP_CLOSURE.ANCESTOR_ID.notIn(reqIdsWithMilestone)))
+			.fetch(RLN_RELATIONSHIP_CLOSURE.ANCESTOR_ID, Long.class);
 	}
 
 	// *************************************** send stuff to abstract workspace ***************************************
@@ -133,91 +152,6 @@ public class RequirementWorkspaceDisplayService extends AbstractWorkspaceDisplay
 	protected String getFolderName() {
 		return "RequirementFolder-";
 	}
-
-//	@Override
-//	protected Map<Long, JsTreeNode> doFindLibraries(List<Long> readableProjectIds, UserDto currentUser, List<Long> openedLibraryIds, Map<Long, JsTreeNode> expandedJsTreeNodes, JsonMilestone activeMilestone) {
-//		List<Long> filteredProjectIds;
-//		if (hasActiveFilter(currentUser.getUsername())) {
-//			filteredProjectIds = findFilteredProjectIds(readableProjectIds, currentUser.getUsername());
-//		} else {
-//			filteredProjectIds = readableProjectIds;
-//		}
-//
-//		Table<?> childrenInfo = DSL
-//			.select(selectLibraryId(),
-//				org.jooq.impl.DSL.groupConcat(org.jooq.impl.DSL.concat(RLC.CONTENT_ID.cast(String.class), "=Sep="))
-//					.orderBy(RLC.CONTENT_ORDER).as("CHILDREN_ID"),
-//				org.jooq.impl.DSL.groupConcat(org.jooq.impl.DSL.concat(org.jooq.impl.DSL.decode()
-//					.when(RCF.RLN_ID.isNotNull(), "requirement-folders")
-//					.otherwise("requirements"), "=Sep="))
-//					.orderBy(RLC.CONTENT_ORDER).as("CHILDREN_CLASS")
-////				org.jooq.impl.DSL.groupConcat(org.jooq.impl.DSL.concat(RES.NAME, "=Sep="))
-////					.orderBy(RLC.CONTENT_ORDER).as("CHILDREN_NAME"),
-////				org.jooq.impl.DSL.groupConcat(org.jooq.impl.DSL.concat(org.jooq.impl.DSL.decode()
-////					.when(groupedTCLNR.field("ANCESTOR_ID").isNull(), "false")
-////					.otherwise("true"), "=Sep="))
-////					.orderBy(TCLC.CONTENT_ORDER).as("CHILDREN_HAS_CONTENT")
-//			)
-//			.from(getLibraryTable())
-//			.join(PROJECT).using(selectLibraryId())
-//			.leftJoin(RLC).on(selectLibraryId().eq(RLC.LIBRARY_ID))
-// 			.leftJoin(RLN).on(RLN.RLN_ID.eq(RLC.CONTENT_ID))
-//			.leftJoin(RCF).on(RCF.RLN_ID.eq(RLC.CONTENT_ID))
-////			.leftJoin(TC).on(TCLC.CONTENT_ID.eq(TC.TCLN_ID))
-////			.leftJoin(groupedTestCaseStep).on(TC.TCLN_ID.eq(groupedTestCaseStep.field("TEST_CASE_ID", Long.class)))
-////			.leftJoin(RVC).on(TCLC.CONTENT_ID.eq(RVC.VERIFYING_TEST_CASE_ID))
-////			.leftJoin(groupedTCLNR).on(TCLC.CONTENT_ID.eq(groupedTCLNR.field("ANCESTOR_ID", Long.class)))
-//			.where(PROJECT.PROJECT_ID.in(openedLibraryIds))
-//			.and(PROJECT.PROJECT_TYPE.eq(PROJECT_TYPE))
-//			.groupBy(selectLibraryId())
-//			.asTable("CHILDREN_INFO");
-//
-//		Map<Long, JsTreeNode> jsTreeNodes = DSL
-//			.select(selectLibraryId(),
-//				PROJECT.PROJECT_ID,
-//				PROJECT.NAME,
-//				PROJECT.LABEL,
-//				childrenInfo.field("CHILDREN_ID"),
-//				childrenInfo.field("CHILDREN_CLASS")
-//			)
-//			.from(getLibraryTable())
-//			.join(PROJECT).using(selectLibraryId())
-//			.leftJoin(childrenInfo).using(selectLibraryId())
-//			.where(PROJECT.PROJECT_ID.in(filteredProjectIds))
-//			.and(PROJECT.PROJECT_TYPE.eq(PROJECT_TYPE))
-//			.fetch()
-//			.stream()
-//			.map(r -> {
-//				JsTreeNode node = new JsTreeNode();
-//				Long libraryId = r.get(selectLibraryId(), Long.class);
-//				node.addAttr("resId", libraryId);
-//				node.setTitle(r.get(PROJECT.NAME));
-//				node.addAttr("resType", getResType());
-//				node.addAttr("rel", getRel());
-//				node.addAttr("name", getClassName());
-//				node.addAttr("id", getClassName() + '-' + libraryId);
-//				node.addAttr("title", r.get(PROJECT.LABEL));
-//				node.addAttr("project", r.get(PROJECT.PROJECT_ID));
-//
-//				//permissions set to false by default except for admin witch have rights by definition
-//				EnumSet<PermissionWithMask> permissions = EnumSet.allOf(PermissionWithMask.class);
-//				for (PermissionWithMask permission : permissions) {
-//					node.addAttr(permission.getQuality(), String.valueOf(currentUser.isAdmin()));
-//				}
-//
-//				// milestone attributes : libraries are yes-men
-//				node.addAttr("milestone-creatable-deletable", "true");
-//				node.addAttr("milestone-editable", "true");
-//				node.addAttr("wizards", new HashSet<String>());
-//				node.setState(JsTreeNode.State.closed);
-//				return node;
-//			})
-//			.collect(Collectors.toMap(node -> (Long) node.getAttr().get("resId"), Function.identity()));
-//
-//		//TODO opened nodes and content
-//		return jsTreeNodes;
-//	}
-
 
 	@Override
 	protected String getRel() {
