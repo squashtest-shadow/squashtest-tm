537652152bf858e95f3416d79befecf4f47b97c9
Grelotine <Grelotine@localhost>
2017-11-03 14:51:31 +0100

    Sonar - Placed string literal on te left side of string comparison

--- tm/tm.web/src/main/java/org/squashtest/tm/web/internal/model/datatable/SpringPagination.java
+++ tm/tm.web/src/main/java/org/squashtest/tm/web/internal/model/datatable/SpringPagination.java
@@ -39,67 +39,67 @@ import org.squashtest.tm.web.internal.model.viewmapper.DatatableMapper;
  * @author bsiri
  */
 public class SpringPagination {
-    
+
     private SpringPagination(){
         super();
     }
-    
+
     /**
     * Will create a Pageable object from the DataTableDrawParameter in arguments.
-    * In this version, the Pageable will only consider the pagination but will 
-    * not include sorting features 
+    * In this version, the Pageable will only consider the pagination but will
+    * not include sorting features
     * (you would rather consider {#link pageable(DataTableDrawParameters, DatatableMapper)}
-    * 
+    *
     * @param dtParams the parameters sent by the datatable (client-size)
     */
     public static Pageable pageable(DataTableDrawParameters dtParams){
 
         PageNumSize pns = createPageNumSize(dtParams);
         return new PageRequest(pns.pagenum, pns.pagesize);
-        
+
     }
-    
+
     /**
      * Will create a Pageable object from the DataTableDrawParameter and DatatableMapper in arguments.
      * This will include the pagination information, and also if applicable the directives about data sorting.
-     * 
+     *
      */
     public static Pageable pageable(DataTableDrawParameters dtParams, DatatableMapper mapper){
-        
+
         // 1 - paging
         PageNumSize pns = createPageNumSize(dtParams);
-        
+
         // 2 - sorting
         Sort sort = createSort(dtParams, mapper);
-        
+
         return new PageRequest(pns.pagenum, pns.pagesize, sort);
 
     }
-    
-    
+
+
     private static PageNumSize createPageNumSize(DataTableDrawParameters dtParams){
-                
-        // Default to no pagination (ie should display all results)        
+
+        // Default to no pagination (ie should display all results)
         // this is the official trick from Spring community to emulate 'infinity'
-        int pagesize = Integer.MAX_VALUE;   
+        int pagesize = Integer.MAX_VALUE;
         int pagenum = 0;
-        
+
         // in case we actually want pagination :
         // page size = page size
         // page number = first item index / page size
         // note : normally only integer division will occur, no need to worry about float result
         if (dtParams.getiDisplayLength() > 0){
             pagesize = dtParams.getiDisplayLength();
-            pagenum = dtParams.getiDisplayStart() / pagesize;            
+            pagenum = dtParams.getiDisplayStart() / pagesize;
         }
-        
+
         return new PageNumSize(pagenum, pagesize);
     }
-    
+
     private static Sort createSort(DataTableDrawParameters dtParams, DatatableMapper mapper){
         Sort sort = null;
         int howMany = dtParams.getiSortingCols();
-        
+
         if ( howMany > 0){
             List<Order> orders = new ArrayList<>(howMany);
 
@@ -109,23 +109,23 @@ public class SpringPagination {
             String attribute;
 
             for (int i=0; i < howMany; i++){
-                
+
                 sortedcol = dtParams.getiSortCol(i);
                 mappingkey = dtParams.getmDataProp(sortedcol);
                 attribute = mapper.getMapping(mappingkey);
-                
+
                 sorteddir = dtParams.getsSortDir(i);
-                Direction direction = (sorteddir.equals("asc")) ? Direction.ASC : Direction.DESC;
-                
-                orders.add(new Order(direction, attribute));                
+                Direction direction = ("asc".equals(sorteddir)) ? Direction.ASC : Direction.DESC;
+
+                orders.add(new Order(direction, attribute));
             }
-            
+
             sort = new Sort(orders);
-        }  
+        }
         return sort;
     }
-        
-    // IGNOREVIOLATIONS:START 
+
+    // IGNOREVIOLATIONS:START
     private static final class PageNumSize{
         int pagenum;
         int pagesize;
@@ -134,5 +134,5 @@ public class SpringPagination {
             this.pagesize = pagesize;
         }
     }
-    // IGNOREVIOLATIONS:END 
+    // IGNOREVIOLATIONS:END
 }
